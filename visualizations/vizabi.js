ce17a0b95a79de345721dc7403b39ee738a91e9d
Thiago Costa Porto <thiagocostaporto@gmail.com>
2014-05-28 01:20:11 +0200

    compact vizabi class gen

--- visualizations/vizabi.js
+++ visualizations/vizabi.js
@@ -1,35 +1,28 @@
 define([
     'd3',
     'vizabi.base.object',
-    'vizabi.base.svg.rectBox'
-], function(d3, object, RectBox) {
+    'vizabi.base.svg.rectBox',
+    'visualizations/dd/dd'
+], function(d3, object, RectBox, dd) {
     var extend = object.extend;
 
     var template = function(core, options) {
-        this.id = options.id || 'id-undefined';
-        this.selector = options.selector || 'body';
+        this.id = (options && options.name) || 'id-undefined';
+        this.selector = (options && options.selector) || 'body';
 
-        // The visualization *state*. This contains the properties of the
-        // visualization that is being displayed to the user.
-        this.state = {
-
-        };
-
-        // The language of this visualization (*strongly suggested to exist*)
+        this.state = {};
         this.language = 'dev';
 
         // SVG builder
-        this.wrapperName = 'vizabi-bare-minimum-' + this.id;
+        this.wrapperName = 'vizabi-' + this.id;
 
         this.container = d3.select(this.selector).append('div')
-            .attr('id', this.wrapperName)
-            .attr('class', 'bare-minimum');
+            .attr('id', this.wrapperName);
 
-        this.svg = d3.select('#' + this.wrapperName).append('svg')
-            .attr('class', 'bare-minimum');
+        this.svg = d3.select('#' + this.wrapperName).append('svg');
 
         // Manager instances, obtained from the core
-        this.instances = {
+        this.managers = {
             events: core.getInstance('events'),
             data: core.getInstance('data'),
             layout: core.getInstance('layout'),
@@ -37,70 +30,29 @@ define([
         };
 
         // Sets up Managers
-        this.instances.layout.setProperties({
+        this.managers.layout.setProperties({
             div: this.container,    // d3 object
             svg: this.svg,          // d3 object
             schema: 'desktop',
             defaultMeasures: { width: 900, height: 500 }
         });
 
-        // Tools are used by widgets
-        this.tools = {
+        var _this = this;
 
-        };
-
-        // Widgets that are used by the visualization. On this template,
-        // we load the 'text' and the axis Widgets.
-        this.widgets = {
-            
-        };
+        this.managers.events.bind('resize', function() {
+            _this.managers.layout.update();
+        });
     };
 
     template.prototype = {
         start: function() {
-            // Layout positioning for the layout manager
-            this.layout = {
-                desktop: {
-                    
-                }
-            };
-
-            // Set the layout
-            this.setLayout(this.layout);
-
-            // Build tools if necessary
-            //this.tools.TOOL.build();
-            
-            // Start Widgets
-            //this.widgets.WIDGET.start();
-
-            // Bindings
-            var _this = this;
-
-            this.instances.events.bind('change:state', function(state) {
-                
-            });
-
-            this.instances.events.bind('change:language', function(lang) {
-                
-            });
-
-            this.instances.events.bind('resize', function() {
-                _this.instances.layout.update();
-            });
-
-            // Run the layout manager for the first time
-            this.instances.layout.update();
-
             // returns self
-
-            console.log('este carro esta sendo roubado');
             return this;
         },
 
         // Ignore
         stop: function() {
-            this.instances.events.trigger('interactive:halt');
+            this.managers.events.trigger('vizabi:halt');
             return this;
         },
 
@@ -119,14 +71,14 @@ define([
 
         setState: function(state) {
             extend(this.state, state);
-            this.instances.events.trigger('change:state', this.state);
+            this.managers.events.trigger('change:state', this.state);
             return this;
         },
 
         setLanguage: function(lang) {
             if (!lang) return;  // let it fail silently?
-            this.instances.i18n.setLanguage(lang, function(lang) {
-                this.instances.events.trigger('change:language', lang);
+            this.managers.i18n.setLanguage(lang, function(lang) {
+                this.managers.events.trigger('change:language', lang);
             });
             return this;
         },
@@ -142,37 +94,19 @@ define([
         setLayout: function(layout) {
             if (!layout) return this;   // let it fail silently.
             this.layout = layout;
-            this.instances.layout.set(layout);
+            this.managers.layout.set(layout);
             return this;
         },
 
         getInstance: function(name) {
-            if (name === 'events') {
-                return this.instances.events;
-            } else if (name === 'data') {
-                return this.instances.data;
-            } else if (name === 'layout') {
-                return this.instances.layout;
-            } else if (name === 'i18n') {
-                return this.instances.i18n;
-            } else {
-                return this.instances;
-            }
+            if (!name) return this.managers;
+            if (this.managers[name]) return this.managers[name];
+            return null;
         },
 
-        getProperties: function() {
-            return { tools: this.tools, widgets: this.widgets };
-        },
-
-        setProperties: function(properties) {
-            if (!properties) return this;   // let it fail silently?!
-            
-            // extend property by property
-            if (properties.tools) extend(this.tools, properties.tools);
-            if (properties.widgets) extend(this.widgets, properties.widgets);
-
-            // Add whatever other conditions you want to set
-
+        setContainerClass: function(cssClass) {
+            this.container.classed(cssClass, true);
+            this.svg.classed(cssClass, true);
             return this;
         }
     };
