744026c9f2bb1403a06bded259f3a65936ba859d
Thiago Costa Porto <thiagocostaporto@gmail.com>
2014-05-30 14:46:46 +0200

    bostock’s bar chart example

--- visualizations/bar-chart/bar-chart.js
+++ visualizations/bar-chart/bar-chart.js
@@ -10,72 +10,165 @@ define([
         // The visualization *state*. This contains the properties of the
         // visualization that is being displayed to the user.
         barChart.state = {
-        
+            data: undefined
         };
 
         // The language of this visualization (*strongly suggested to exist*)
         barChart.language = 'dev';
 
-        barChart.setLayout({
-            desktop: {
+        var chart = barChart.getSVG().append('g').attr('id', 'chart');
+        var xGroup = barChart.getSVG().append('g').attr('id', 'x axis');
+        var yGroup = barChart.getSVG().append('g').attr('id', 'y axis');
+        var x;
+        var y;
 
+        var pushElement = function (svg) {
+            if (svg.node().getBBox().x < 0) {
+                var offset = -svg.node().getBBox().x;
+                svg.select('g').attr('transform', 'translate(' + offset + ',0)');
+            } else if (svg.node().getBBox().y < 0) {
+                var offset = - svg.node().getBBox().y;
+                svg.select('g').attr('transform', 'translate(0,' + offset + ')');
             }
-        });
+        };
 
-        barChart.start = function() {
-            var margin = {top: 20, right: 30, bottom: 30, left: 40},
-            width = 960 - margin.left - margin.right,
-            height = 500 - margin.top - margin.bottom;
+        var renderYAxis = function(width, height) {
+            if (!barChart.data) return;
 
-            var x = d3.scale.ordinal()
-                .rangeRoundBands([0, width], .1);
+            yGroup.selectAll('g').remove();
 
-            var y = d3.scale.linear()
+            height = +height || 500;
+
+            y = d3.scale.linear()
                 .range([height, 0]);
 
-            var xAxis = d3.svg.axis()
-                .scale(x)
-                .orient("bottom");
+            var max = d3.max(barChart.data, function(d) { return d.value; });
+
+            y.domain([0, max]);
 
             var yAxis = d3.svg.axis()
                 .scale(y)
                 .orient("left");
 
-            var chart = this.getSVG()
-                .attr("width", width + margin.left + margin.right)
-                .attr("height", height + margin.top + margin.bottom)
-              .append("g")
-                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+            yGroup.append("g")
+              .attr("class", "y axis")
+              .call(yAxis);
 
-            d3.tsv("data.tsv", type, function(error, data) {
-                console.log(data)
-              x.domain(data.map(function(d) { return d.name; }));
-              y.domain([0, d3.max(data, function(d) { return d.value; })]);
+            pushElement(yGroup);
+        }.bind(barChart);
 
-              chart.append("g")
-                  .attr("class", "x axis")
-                  .attr("transform", "translate(0," + height + ")")
-                  .call(xAxis);
+        var renderXAxis = function(width) {
+            if (!barChart.data) return;
 
-              chart.append("g")
-                  .attr("class", "y axis")
-                  .call(yAxis);
+            xGroup.selectAll('g').remove();
 
-              chart.selectAll(".bar")
-                  .data(data)
-                .enter().append("rect")
-                  .attr("class", "bar")
-                  .attr("x", function(d) { return x(d.name); })
-                  .attr("y", function(d) { return y(d.value); })
-                  .attr("height", function(d) { return height - y(d.value); })
-                  .attr("width", x.rangeBand());
-            });
+            width = +width || 900;
 
-            function type(d) {
-              d.value = +d.value; // coerce to number
-              return d;
+            x = d3.scale.ordinal()
+                .rangeRoundBands([0, width], .1);
+
+            x.domain(barChart.data.map(function(d) { return d.name; }));
+
+            var xAxis = d3.svg.axis()
+                .scale(x)
+                .orient("bottom");
+
+            xGroup.append("g")
+              .attr("class", "x axis")
+              .call(xAxis);
+
+            pushElement(xGroup);
+        }.bind(barChart);
+
+        var render = function (width, height) {
+            if (!barChart.data) return;
+
+            chart.selectAll('.bar').remove();
+
+            width = +width || 900;
+            height = +height || 500;
+
+            chart.selectAll(".bar")
+              .data(barChart.data)
+            .enter().append("rect")
+              .attr("class", "bar")
+              .attr("x", function(d) { return x(d.name); })
+              .attr("y", function(d) { return y(d.value); })
+              .attr("height", function(d) { return height - y(d.value); })
+              .attr("width", x.rangeBand());
+        }.bind(barChart);
+
+        d3.tsv("data.tsv", type, function(error, data) {
+          barChart.data = data;
+          renderXAxis();
+          renderYAxis();
+          render();
+          barChart.managers.layout.update();
+        });
+
+        function type(d) {
+          d.value = +d.value; // coerce to number
+          return d;
+        }
+
+        render();
+
+        barChart.setLayout({
+            desktop: {
+                yAxis: {
+                    element: yGroup,
+                    render: renderYAxis,
+                    top: 20,
+                    bottom: {
+                        parent: 'stage',
+                        anchor: 'height',
+                        padding: -20
+                    },
+                    left: 20
+                },
+                xAxis: {
+                    element: xGroup,
+                    render: renderXAxis,
+                    top: {
+                        parent: 'yAxis',
+                        anchor: 'bottom'
+                    },
+                    left: {
+                        parent: 'yAxis',
+                        anchor: 'right'
+                    },
+                    right: {
+                        parent: 'stage',
+                        anchor: 'width'
+                    }
+                },
+                chart: {
+                    element: chart,
+                    render: render,
+                    top: {
+                        parent: 'yAxis',
+                        anchor: 'top'
+                    },
+                    bottom: {
+                        parent: 'yAxis',
+                        anchor: 'bottom'
+                    },
+                    left: {
+                        parent: 'xAxis',
+                        anchor: 'left'
+                    },
+                    right: {
+                        parent: 'xAxis',
+                        anchor: 'right'
+                    }
+                }
             }
+        });
 
+        // console.log('here');
+        barChart.managers.layout.update();
+
+        barChart.start = function() {
             return this;
         };
 
