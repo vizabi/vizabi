dd29a6815fad13bec914e59490d35fc0570a4b62
Thiago Costa Porto <thiagocostaporto@gmail.com>
2014-06-03 11:02:04 +0200

    yo generator for tools

---
+++ generator-vizabi/app/templates/_tool_plain.js
@@ -0,0 +1,33 @@
+define([
+    'tools/tool'
+], function(Tool) {
+    var newVizabi = function(core, options) {
+        var <%= _.camelize(vizabiName) %> = new Tool(core, options);
+
+        <%= _.camelize(vizabiName) %>.name = '<%= _.slugify(vizabiName) %>';
+        <%= _.camelize(vizabiName) %>.getSVG().classed('<%= _.slugify(vizabiName) %>', true);
+
+        // The visualization *state*. This contains the properties of the
+        // visualization that is being displayed to the user.
+        <%= _.camelize(vizabiName) %>.state = {
+        
+        };
+
+        // The language of this visualization (*strongly suggested to exist*)
+        <%= _.camelize(vizabiName) %>.language = 'dev';
+
+        <%= _.camelize(vizabiName) %>.setLayout({
+            desktop: {
+
+            }
+        });
+
+        <%= _.camelize(vizabiName) %>.start = function() {
+            return this;
+        };
+
+        return <%= _.camelize(vizabiName) %>;
+    };
+
+    return newVizabi;
+});
