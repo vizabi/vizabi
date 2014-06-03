8bd2ad8fa0c1d43faded463591e3664e8930e8d5
Arthur Camara <arthurcamara@gmail.com>
2014-06-03 11:36:06 +0200

    Make managers accessible only at core level

--- core/core.js
+++ core/core.js
@@ -2,40 +2,23 @@ define([
     'd3',
     'base/object',
     'vizabi-config',
-<<<<<<< HEAD
-    'managers/events'
-], function(d3, baseObject, config, events) {
-=======
     'managers/events/events',
     'managers/layout/layout',
     'managers/data/data',
     'i18n' 
 ], function(d3, baseObject, config, EventsManager, LayoutManager, DataManager, i18nManager) {
-    // Please remove me soon (when gruntfile is adjusted)
-    if (!window.d3) window.d3 = d3;
->>>>>>> 3710ae349918621da284289e1d6f6cdb2630b6c9
 
     var extend = baseObject.extend;
     var vizID = 1;
-
-    function createVisualization(tool, callback) {
-        var vPath = config.require.paths.visualizations;
-        var vName = tool;
-        var path = vPath + '/' + vName + '/' + vName;
-
-        require([path], function(Viz) {
-            callback(Viz, options);
-        });
-    }
+    var managers = {
+        events: EventsManager,
+        layout: LayoutManager,
+        data: DataManager,
+        i18n: i18nManager
+    };
 
     var core = function() {
         this.visualizations = {};
-        this.managers = {
-            events: EventsManager,
-            layout: LayoutManager,
-            data: DataManager,
-            i18n: i18nManager
-        };
         
         var context = this;
         
@@ -50,7 +33,7 @@ define([
         },
 
         getInstance: function(manager) {
-            return this.managers[manager].instance();
+            return managers[manager].instance();
         },
 
         getVisualization: function(id) {
@@ -59,17 +42,17 @@ define([
             return null;
         },
 
-        start: function(tool, placeholder, state) {
+        start: function(tool_name, placeholder, state) {
 
             var id = this.getId();
             var context = this;
             var opt = extend({}, options);
 
             var t_path = config.require.paths.visualizations;
-            var path = t_path + '/_examples/' + tool + '/' + tool;
+            var path = t_path + '/' + tool_name + '/' + tool_name;
 
-            require([path], function(Viz) {
-                callback(Viz, options);
+            require([path], function(tool) {
+                //TODO tool.start();
             });
 
             return id;
@@ -79,12 +62,12 @@ define([
             
         },
         bind: function(evt, func) {
-            this.managers['events'].bind(evt, func);
+            EventsManager.bind(evt, func);
         },
 
         trigger: function(what) {
             var args = Array.prototype.slice.call(arguments).slice(1);
-            this.managers['events'].trigger(what, args);
+            EventsManager.trigger(what, args);
         }
     };
 
