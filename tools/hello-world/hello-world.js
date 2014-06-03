9cecd30f1f956325f1c8138339f6c22727bcf737
ambodi <amir.rahnama@gmail.com>
2014-06-03 18:56:13 +0200

    Hello world extends tool

--- tools/hello-world/hello-world.js
+++ tools/hello-world/hello-world.js
@@ -1,58 +1,27 @@
 define([
+    'underscore',
     'tools/tool'
-], function(Tool) {
+], function(_, Tool) {
 
     var helloWorld = Tool.extend({
         init: function(context, options) {
-          //this._super("Testing");
+            this.name = 'hello-world';
+            this.placeholder = options.placeholder;
+            this.state = _.extend({
+                show: {
+                    'world': {
+
+                    }
+                },
+                time: '1980',
+                yaxis: {
+                    indicator: 'gdp'
+                }
+            }, options.state);
+
+            this._super(options);
         }
     });
 
     return helloWorld;
-
-
-
-    var newVizabi = function(core, options) {
-
-        var helloWorld = Tool.extend({
-            init: function(context, options) {
-              console.log("HELLO WORLD!!!");
-            }
-        });
-
-        return helloWorld;
-
-       
-        /* Initialization part */
-
-        helloWorld.name = 'hello-world';
-        helloWorld.getSVG().classed('hello-world', true);
-
-        // Default state
-        helloWorld.state = {
-            show: {
-                'world': {
-
-                }
-            },
-            time: '1980',
-            yaxis: {
-                indicator: 'gdp'
-            }
-        };
-
-        helloWorld.widgets = {}
-
-        var parent = helloWorld.start;
-        helloWorld.start(function() {
-            return this;
-        });
-
-
-        helloWorld.start();
-
-        return helloWorld;
-    };
-
-    return newVizabi;
 });
