a15d383358f9fab1538d667651a896eefc665ff2
Arthur Camara <arthurcamara@gmail.com>
2014-06-04 00:06:51 +0200

    Improve build experience with new grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,89 @@
+
+
 module.exports = function (grunt) {
+
+    /* 
+     * load all grunt tasks, instead of loading each one like this:
+     * grunt.loadNpmTasks('grunt-concurrent'); ...
+     * This reads the file package.json
+     * More info here: https://github.com/sindresorhus/load-grunt-tasks
+     */
+    require('load-grunt-tasks')(grunt);
+
     grunt.initConfig({
+
+      // Clean dist folder to have a clean start
+      clean: {
+          dist: ["dist"]
+      },
+
+      // Copy all js files to dist folder
+      copy: {
+          build: {
+            cwd: 'src',
+            src: [ '**/*.js', '**/*.html' ],
+            dest: 'dist',
+            expand: true
+          },
+      },
+
+      // Uglifying JS files
+      uglify: {
+        files: { 
+            cwd: 'src/',          // base path
+            src: '**/*.js',   // source files mask
+            dest: 'dist',         // destination folder
+            expand: true,         // allow dynamic building
+            mangle: false,        // disallow change in names
+            flatten: false        // remove all unnecessary nesting
+        }
+      },
+
+      // Compile SCSS files into CSS (dev mode is not compressed)
       sass: {
         dist: {
+          options: {
+            style: 'compressed'
+          },
+          files: {
+            'dist/vizabi.css': 'src/style/vizabi.scss',
+          }
+        },
+        dev: {
           options: {
             style: 'expanded'
           },
           files: {
-            'visualizations/vizabi.css': 'visualizations/vizabi.scss',
+            'dist/vizabi.css': 'src/style/vizabi.scss',
           }
         }
-      }
+      },
+
     });
 
-    grunt.loadNpmTasks('grunt-contrib-sass');
+    /* 
+     * -----------------------------
+     * Tasks:
+     */
 
-    grunt.registerTask('default', ['sass']);
+    //default task: grunt
+    grunt.registerTask('default',[
+
+      'clean:dist',   //clean dist folder
+      'copy',         //copy js files
+      'uglify',       //uglify js files
+      'sass:dist'     //compile scss
+
+    ]);
+
+    //developer task: grunt dev
+    grunt.registerTask('dev', [
+
+      'clean:dist',   //clean dist folder
+      'copy',         //copy js files
+      'sass:dev'      //compile scss
+
+    ]);
+
+
 }
