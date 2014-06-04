12b16cec966d22244f3013a6d9428b732d0756fb
ambodi <amir.rahnama@gmail.com>
2014-06-04 10:27:25 +0200

    add watch task for templates and scripts file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,104 +1,109 @@
+module.exports = function(grunt) {
 
+  /* 
+   * load all grunt tasks, instead of loading each one like this:
+   * grunt.loadNpmTasks('grunt-concurrent'); ...
+   * This reads the file package.json
+   * More info here: https://github.com/sindresorhus/load-grunt-tasks
+   */
+  require('load-grunt-tasks')(grunt);
 
-module.exports = function (grunt) {
+  grunt.initConfig({
 
-    /* 
-     * load all grunt tasks, instead of loading each one like this:
-     * grunt.loadNpmTasks('grunt-concurrent'); ...
-     * This reads the file package.json
-     * More info here: https://github.com/sindresorhus/load-grunt-tasks
-     */
-    require('load-grunt-tasks')(grunt);
+    // Clean dist folder to have a clean start
+    clean: {
+      dist: ["dist"]
+    },
 
-    grunt.initConfig({
-
-      // Clean dist folder to have a clean start
-      clean: {
-          dist: ["dist"]
+    // Copy all js and template files to dist folder
+    copy: {
+      scripts: {
+        cwd: 'src',
+        src: ['**/*.js'],
+        dest: 'dist',
+        expand: true
       },
+      templates: {
+        cwd: 'src',
+        src: ['**/*.html'],
+        dest: 'dist',
+        expand: true
+      }
+    },
 
-      // Copy all js and template files to dist folder
-      copy: {
-            scripts: {
-              cwd: 'src',
-              src: ['**/*.js'],
-              dest: 'dist',
-              expand: true
-            },
-            templates: {
-              cwd: 'src',
-              src: ['**/*.html'],
-              dest: 'dist',
-              expand: true
-            }            
-          },
-      },
+    // Uglifying JS files
+    uglify: {
+      files: {
+        cwd: 'src/',     // base path
+        src: '**/*.js',  // source files mask
+        dest: 'dist',    // destination folder
+        expand: true,    // allow dynamic building
+        mangle: false,   // disallow change in names
+        flatten: false   // remove all unnecessary nesting
+      }
+    },
 
-      // Uglifying JS files
-      uglify: {
-        files: { 
-            cwd: 'src/',          // base path
-            src: '**/*.js',       // source files mask
-            dest: 'dist',         // destination folder
-            expand: true,         // allow dynamic building
-            mangle: false,        // disallow change in names
-            flatten: false        // remove all unnecessary nesting
-        }
-      },
-
-      // Compile SCSS files into CSS (dev mode is not compressed)
-      sass: {
-        dist: {
-          options: {
-            style: 'compressed'
-          },
-          files: {
-            'dist/vizabi.css': 'src/style/vizabi.scss',
-          }
+    // Compile SCSS files into CSS (dev mode is not compressed)
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
         },
-        dev: {
-          options: {
-            style: 'expanded'
-          },
-          files: {
-            'dist/vizabi.css': 'src/style/vizabi.scss',
-          }
+        files: {
+          'dist/vizabi.css': 'src/style/vizabi.scss',
         }
       },
-
-      // Make sure necessary files are built when changes are made
-      watch: {
-        styles: {
-          files: ['src/**/*.scss'],
-          tasks: ['sass:dev']
+      dev: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'dist/vizabi.css': 'src/style/vizabi.scss',
         }
       }
-    });
+    },
 
-    /* 
-     * -----------------------------
-     * Tasks:
-     */
+    // Make sure necessary files are built when changes are made
+    watch: {
+      styles: {
+        files: ['src/**/*.scss'],
+        tasks: ['sass:dev']
+      },
+      scripts: {
+        files: ['src/**/*.js'],
+        tasks: ['copy:scripts']
+      },
+      templates: {
+        files: ['src/**/*.html'],
+        tasks: ['copy:templates']
+      }
+    }
+  });
 
-    //default task: grunt
-    grunt.registerTask('default',[
+/* 
+ * -----------------------------
+ * Tasks:
+ */
 
-      'clean:dist',   //clean dist folder
-      'copy:templates',         //copy js and template files
-      'uglify',       //uglify js files
-      'sass:dist'     //compile scss
+//default task: grunt
+grunt.registerTask('default', [
 
-    ]);
+  'clean:dist',     //clean dist folder
+  'copy:templates', //copy js and template files
+  'uglify',         //uglify js files
+  'sass:dist'       //compile scss
 
-    //developer task: grunt dev
-    grunt.registerTask('dev', [
+]);
 
-      'clean:dist',   //clean dist folder
-      'copy',         //copy js and template files
-      'sass:dev',     //compile scss
-      'watch:styles'
+//developer task: grunt dev
+grunt.registerTask('dev', [
 
-    ]);
+  'clean:dist', //clean dist folder
+  'copy',       //copy js and template files
+  'sass:dev',   //compile scss
+  'watch'       //watch for code changes
+
+]);
 
 
 }
