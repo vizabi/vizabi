f6ebf84f955d7fc3c4f8d27d4e8c140dfe6a41e0
ambodi <amir.rahnama@gmail.com>
2014-06-05 15:15:55 +0200

    Add render and resize to buttonlist components

--- src/components/buttonlist/buttonlist.js
+++ src/components/buttonlist/buttonlist.js
@@ -40,8 +40,55 @@ define([
                 }]
             };
 
-            this._super(core,options);
+            this._super(core, options);
         },
+
+        render: function() {
+            var _this = this;
+            // Return the defer from the superclass 
+            return this._super(function() {
+                //TODO: refactor this callback into separate function
+                _this.placeholder = utils.d3ToJquery(_this.placeholder);
+            });
+
+        },
+
+        resize: function() {
+            var $buttons = this.placeholder.find('#buttonlist .button'),
+                $button_more = this.placeholder.find('#buttonlist #button-more'),
+                offset = 30;
+
+            var size_button = {
+                width: $buttons.first().outerWidth(),
+                height: $buttons.first().outerHeight()
+            };
+            
+            var size_container = {
+                    width: this.placeholder.outerWidth(),
+                    height: this.placeholder.outerHeight()
+                },
+                vertical = size_container.width < size_container.height,
+                number_buttons = $buttons.length - 1,
+                compare = (vertical) ? "height" : "width";
+
+            offset = size_button[compare] / 2;
+            
+            if ((number_buttons * size_button[compare]) <= size_container[compare] - offset) {
+                $buttons.removeClass('hidden');
+                $button_more.addClass('hidden');
+            } 
+            else {
+                var max = Math.floor((size_container[compare] - offset) / size_button[compare]) - 1,
+                    i = 0;
+                $buttons.addClass('hidden');
+                $buttons.each(function() {
+                    i++;
+                    $(this).removeClass('hidden');
+                    if (i >= max) return false;
+                });
+                $button_more.removeClass('hidden');
+            }
+        }
 
     });
 
