942184df46515446b17de21ea97004618a6ca3bc
ambodi <amir.rahnama@gmail.com>
2014-06-27 00:16:55 +0200

    Renamed previous time slider to slider & made timeslider2 that holds slider and play button
    VIZ-471

---
+++ src/components/slider/slider.js
@@ -0,0 +1,76 @@
+//TODO: refactor this whole thing
+//TODO: timeslider is composed of jquery button and jquery slider and packed into one, since they can extend 
+
+//TODO: differentiate between chart componenst and others by viz- word
+
+define([
+    'jquery',
+    'base/utils',
+    'components/component',
+    'jqueryui_slider'
+], function($, utils, Component) {
+
+	var container,
+		timeslider,
+		handle;
+
+    var Timeslider = Component.extend({
+        init: function(parent, options) {
+            this.template = "components/slider/slider";
+
+            // Same constructor as the superclass
+            this._super(parent, options);
+
+            range = this.model.getState("timeRange");
+            startYear = this.model.getState("time");
+        },
+
+        postRender: function() {
+            this.placeholder = utils.d3ToJquery(this.placeholder);
+            container = utils.d3ToJquery(this.element);
+            //create timeslider
+            timeslider = container.find("#slider");
+            timeslider.slider();
+            handle = timeslider.find(".ui-slider-handle");
+
+            this.update();
+        },
+
+
+        resize: function() {
+            this.update();
+        },
+
+        update: function() {
+           var _this = this,
+           	   year = this.model.getState("time");
+
+           timeslider.slider({
+           		min: this.model.getState("timeRange")[0],
+           		max: this.model.getState("timeRange")[1],
+           		value: year,
+           		slide: function(evt, ui) {
+           			_this.setYear(ui.value);
+           		}
+           });
+
+           handle.attr("data-year", year);
+        },
+
+        getYear: function() {
+            return this.model.getState("time");
+        },
+
+        setYear: function(year, silent) {
+            //update state
+            this.model.setState({
+                time: year
+            }, silent);
+
+            this.update();
+        },
+
+    });
+
+    return Timeslider;
+});
