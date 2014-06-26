1caff1747a77a6b5a4e8c20184c4a8b6ab2421c3
ambodi <amir.rahnama@gmail.com>
2014-06-27 00:18:29 +0200

    Add play button component

---
+++ src/components/play-button/play-button.js
@@ -0,0 +1,60 @@
+define([
+    'jquery',
+    'base/utils',
+    'components/component',
+], function($, utils, Component) {
+
+    var container,
+        play,
+        pause,
+        playInterval;
+
+    var PlayButton = Component.extend({
+        init: function(parent, options) {
+            this.template = "components/play-button/play-button";
+            this._super(parent, options);
+        },
+
+        postRender: function() {
+            var _this = this;
+            container = utils.d3ToJquery(this.element);
+
+            play = container.find("#play-button-play"),
+            pause = container.find("#play-button-pause");
+
+            play.click(function() {
+                _this.play();
+            });
+
+            pause.click(function() {
+                _this.pause();
+            });
+        },
+
+        play: function() {
+            container.addClass("playing");
+            var _this = this,
+                yearValue = this.model.getState("time"),
+                range = this.model.getState("timeRange");
+                
+            playInterval = setInterval(function() {
+                if (yearValue > range[1]) {
+                    _this.pause();
+                    return;
+                } else {
+                    _this.model.setState({
+                        time: yearValue++
+                    });
+                }
+            }, 200);
+        },
+
+        pause: function() {
+            container.removeClass("playing");
+            clearInterval(playInterval);
+        }
+
+    });
+
+    return PlayButton;
+});
