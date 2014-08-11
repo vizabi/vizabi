54efeaf0bcc0e3eef1a943ef49052d19d1034f55
Arthur Camara <arthurcamara@gmail.com>
2014-08-11 09:41:21 +0200

    Read new waffle format

--- src/readers/local-json.js
+++ src/readers/local-json.js
@@ -1,42 +1,94 @@
  define([
-    'base/class',
-    'jquery',
-    'underscore'
-], function(Class, $, _) {
+     'base/class',
+     'jquery',
+     'underscore'
+ ], function(Class, $, _) {
 
-    var LocalJSONReader = Class.extend({
+     var LocalJSONReader = Class.extend({
 
-        init: function(basepath) {
-            this.data = {};
-            this.basepath = basepath;
-        },
+         init: function(basepath) {
+             this.data = [];
+             this.basepath = basepath;
+         },
 
-        read: function(options) {
-            var _this = this,
-                defer = $.Deferred();
-            
-            //creating query in the expected object format as discussed 
-            //(local json)
-            var query = {};
+         read: function(queries, language) {
+             var _this = this,
+                 defer = $.Deferred();
 
-            //sending request to server
-            var promise = $.getJSON(_this.basepath, query , function(res) {
-                _this.data = res || {};
-            });
+             var path = this.basepath.replace("{{LANGUAGE}}", language);
 
-            promise.done(function() {
-                defer.resolve();
-            });
+             //sending request to server
+             var promise = $.getJSON(path, function(res) {
+                 _this.data = [];
+                 for (var i in queries) {
+                     _this.data.push(queryJSON(res, queries[i]))
+                 }
+             });
 
-            return defer;
-        },
+             promise.done(function() {
+                 defer.resolve();
+             });
 
-        getData: function() {
-            return this.data;
+             return defer;
+         },
+
+         getData: function() {
+             return this.data;
+         }
+
+     });
+
+     function queryJSON(json, query) {
+         if (query.from == "data") {
+             var table_name = getTableName(query);
+             var data = json.tables[table_name].data;
+             return filterColumns(filterRows(data, query.where), query.select);
+         }
+     }
+
+     function getTableName(query) {
+         var table_name = [];
+         _.each(query.where, function(value, field) {
+             table_name.push(field);
+         });
+         return table_name.join("_");
+     }
+
+     function filterColumns(array, columns) {
+        return _.map(array, function(row) {
+            return _.pick(row, columns);
+        })
+     };
+
+     function filterRows(array, conditions) {
+        return _.filter(array, function(row) {
+                var include = true;
+
+                for(var field in conditions) {
+                    var value = conditions[field];
+                    if(!matchValue(field, value, row[field])) {
+                        include = false;
+                        break;
+                    }
+                }
+                return include;
+             });
+     }
+
+     function matchValue(field, possible, value) {
+
+        if(_.isString(possible)) {
+            if(field === "year" && _.contains(possible, "-")) {
+                var years = possible.split("-");
+                possible = _.range(parseInt(years[0], 10),parseInt(years[1], 10)+1);
+            } else {
+                possible = [possible];
+            }
         }
+        //TODO: review string and int incompatibility
+        return _.contains(possible, value);
+     }
 
-    });
+     return LocalJSONReader;
 
-    return LocalJSONReader;
-
-});
+ });
