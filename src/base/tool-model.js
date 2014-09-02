c98c0e6fbe6f573e9a3bb845e002d5b773b991fe
Arthur Camara <arthurcamara@gmail.com>
2014-09-02 12:26:58 +0200

    Add language model to tool model

--- src/base/tool-model.js
+++ src/base/tool-model.js
@@ -8,6 +8,7 @@ define([
     var ToolModel = Model.extend({
         init: function(data) {
             this.state = new Model();
+            this.language = new Model();
             this.data = new Model();
             this.data.setSource(data);
         },
@@ -31,7 +32,17 @@ define([
             // TODO: Here we do validation
             this.data.set(attr, value, silent);
             if (!silent) Events.trigger("change:data", this.data.get());
-        }
+        },
+
+        getLanguage: function(attr) {
+            return this.language.get(attr);
+        },
+
+        setLanguage: function(attr, value, silent) {
+            // TODO: Here we do validation
+            this.language.set(attr, value, silent);
+            if (!silent) Events.trigger("change:language", this.language.get());
+        },
 
     });
 
