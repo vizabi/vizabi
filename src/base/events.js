931d2a3cc4de5b970e59c615f3d2913d2d9ef5ad
Arthur Camara <arthurcamara@gmail.com>
2014-10-21 10:49:46 +0200

    Trigger event's parent events as well

--- src/base/events.js
+++ src/base/events.js
@@ -2,44 +2,49 @@ define([
     "underscore",
     "base/class"
 ], function(_, Class) {
-    
+
     var Events = Class.extend({
 
         init: function() {
             this.events = {};
         },
 
+        //alias to bind
+        on: function(name, func) {
+            this.bind(name, func);
+        },
+
         //bind a function to a certain event
         bind: function(name, func) {
 
             //bind multiple at a time
-            if(_.isArray(name)) {
+            if (_.isArray(name)) {
                 for (var i = 0; i < name.length; i++) {
                     this.bind(name[i], func);
                 };
                 return;
             }
 
-            if(!_.isArray(this.events[name])) {
+            if (!_.isArray(this.events[name])) {
                 this.events[name] = [];
             }
-            if(_.isFunction(func)) {
+            if (_.isFunction(func)) {
                 this.events[name].push(func);
             } else {
-                throw_msg("Can't bind '"+func+"' to event '"+name+"'. It must be a function!");
+                throw_msg("Can't bind '" + func + "' to event '" + name + "'. It must be a function!");
             }
         },
         unbind: function(name, func) {
 
             //unbind multiple at a time
-            if(_.isArray(name)) {
+            if (_.isArray(name)) {
                 for (var i = 0; i < name.length; i++) {
                     this.unbind(name[i], func);
                 };
                 return;
             }
 
-            if(_.isFunction(func)) {
+            if (_.isFunction(func)) {
                 this.events[name] = _.without(this.events[name], func);
             } else {
                 this.events[name] = [];
@@ -48,20 +53,31 @@ define([
         unbindAll: function() {
             this.events = {};
         },
-        //trigger a certain event, executing each function
-        trigger: function(name, args) {
 
-            if(_.isUndefined(this.events[name])) return;
-            for (var i = 0, size=this.events[name].length; i < size; i++) {
+        //trigger event and all parents. E.g: change:language and change
+        trigger: function(name, args) {
+            var parts = name.split(":"),
+                new_name;
+
+            while (parts.length > 0) {
+                new_name = parts.join(":");
+                this.triggerEach(new_name, args);
+                parts.pop();
+            }
+        },
+        //trigger one specific event
+        triggerEach: function(name, args) {
+            if (_.isUndefined(this.events[name])) return;
+            for (var i = 0, size = this.events[name].length; i < size; i++) {
                 var f = this.events[name][i];
-                if(_.isFunction(f)) {
-                    if(_.isUndefined(args)) {
+                if (_.isFunction(f)) {
+                    if (_.isUndefined(args)) {
                         f();
                     } else {
                         f(args);
                     }
                 } else {
-                    throw_msg("Can't execute '"+func+"' on event '"+name+"'. It must be a function!");
+                    throw_msg("Can't execute '" + func + "' on event '" + name + "'. It must be a function!");
                 }
             };
         }
