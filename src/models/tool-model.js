a88fa14cee17db2ae0ce504d50b77656e0efe7ca
Arthur Camara <arthurcamara@gmail.com>
2014-10-23 01:14:39 +0200

    Improve tool model generation of submodels

--- src/models/tool-model.js
+++ src/models/tool-model.js
@@ -21,67 +21,6 @@ define([
             this.bindEvents(options.bind);
         },
 
-        _generateModelConfig: function(options) {
-
-            var model_config = {},
-                _this = this;
-
-            //todo: improve code below
-            //generate state, data and language models by default
-            model_config.state = this._generateModel("state", options.state)
-            model_config.data = this._generateModel("data", options.data)
-            model_config.bind = this._generateModel("bind", options.bind)
-            model_config.language = this._generateModel("language", options.language);
-            //bind them
-            model_config.state.on("change", function(evt, new_values) {
-                _this.trigger("change:state", new_values);
-            });
-            model_config.data.on("change", function(evt, new_values) {
-                _this.trigger("change:data", new_values);
-            });
-            model_config.bind.on("change", function(evt, new_values) {
-                _this.trigger("change:bind", new_values);
-            });
-            model_config.language.on("change", function(evt, new_values) {
-                _this.trigger("change:language", new_values);
-            });
-
-            //include a model for each property in the state
-            for (var i in options.state) {
-                //naming convention: underscore -> time, time_2, time_overlay
-                var name = i.split("_")[0];
-                model_config[i] = this._generateModel(name, options.state[i]);
-
-                //bind each state submodel to the state
-                model_config[i].on("change", function(evt, new_values) {
-                    _this.get("state").set(i, new_values);
-                    _this.trigger("change:state:" + i, new_values);
-                })
-            }
-
-
-            return model_config;
-        },
-
-        //generate model
-        _generateModel: function(model_name, values) {
-
-            //todo: possible improvement (load via require)
-            var available_models = {
-                    data: DataModel,
-                    language: LanguageModel,
-                    state: StateModel,
-                    time: TimeModel,
-                    bind: Model
-                }
-                //use specific model if it exists
-            if (available_models.hasOwnProperty(model_name)) {
-                return new available_models[model_name](values, this.intervals);
-            } else {
-                return new Model(values, this.intervals);
-            }
-        },
-
         reset: function(new_options, silent) {
             var model_config = this._generateModelConfig(new_options);
             this._super(model_config, silent);
@@ -118,6 +57,64 @@ define([
             for (var i in evts) {
                 _this.on(i, evts[i]);
             }
+        },
+
+        _generateModelConfig: function(options) {
+
+            var model_config = {},
+                _this = this;
+
+            //generate state, data and language models by default and bind
+            var default_models = ["state", "data", "bind", "language"];
+            for (var i=0, size=default_models.length; i<size; i++) {
+                var m = default_models[i];
+                model_config[m] = this._generateModel(m, options[m]);
+                model_config[m].on("change", this._subModelOnChange(m));
+            }
+
+            //include a model for each property in the state and bind
+            for (var i in options.state) {
+                //naming convention: underscore -> time, time_2, time_overlay
+                var name = i.split("_")[0];
+                model_config[i] = this._generateModel(name, options.state[i]);
+                model_config[i].on("change", _this._subStateOnChange(i));
+            }
+
+            return model_config;
+        },
+
+        //generate model
+        _generateModel: function(model_name, values) {
+
+            //todo: possible improvement (load via require)
+            var available_models = {
+                    data: DataModel,
+                    language: LanguageModel,
+                    state: StateModel,
+                    time: TimeModel,
+                    bind: Model
+                }
+                //use specific model if it exists
+            if (available_models.hasOwnProperty(model_name)) {
+                return new available_models[model_name](values, this.intervals);
+            } else {
+                return new Model(values, this.intervals);
+            }
+        },
+
+        _subModelOnChange: function(submodel) {
+            var _this = this;
+            return function(evt, new_values) {
+                _this.trigger("change:"+submodel, new_values);
+            }
+        },
+
+        _subStateOnChange: function(substate) {
+            var _this = this;
+            return function(evt, new_values) {
+                _this.get("state").set(substate, new_values);
+                _this.trigger("change:state:" + substate, new_values);
+            };
         }
 
     });
