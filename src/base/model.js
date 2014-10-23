edf59b067ba01b99af75bcd3066413c5f789d9c5
Arthur Camara <arthurcamara@gmail.com>
2014-10-23 21:38:39 +0200

    Add get in multiple levels to model

--- src/base/model.js
+++ src/base/model.js
@@ -24,8 +24,23 @@ define([
          * Getters and Setters
          */
 
+        //get accepts multiple levels. e.g: get("time.start.value")
         get: function(attr) {
-            return (attr) ? this._data[attr] : this._data;
+            //optimize for common cases
+            if (!attr) return this._data;
+            else if (attr.indexOf('.') === -1) return this._data[attr];
+            //search deeper levels
+            var attrs = attr.split('.'),
+                current = this;
+            while (attrs.length) {
+                curr_attr = attrs.shift();
+                if (typeof current.get === 'function') {
+                    current = current.get(curr_attr);
+                } else {
+                    current = current[curr_attr];
+                }
+            }
+            return current;
         },
 
         //set an attribute for the model, or an entire object
@@ -33,14 +48,14 @@ define([
 
             if (typeof attr !== 'object') {
                 this._data[attr] = (typeof val === 'object') ? _.clone(val) : val;
-                if (!silent) this.events.trigger("change:"+attr, this._data);
+                if (!silent) this.events.trigger("change:" + attr, this._data);
             } else {
                 block_validation = silent;
                 silent = val;
                 for (var att in attr) {
                     var val = attr[att];
                     this._data[att] = (typeof val === 'object') ? _.clone(val) : val;
-                    if (!silent) this.events.trigger("change:"+att, this._data);
+                    if (!silent) this.events.trigger("change:" + att, this._data);
                 }
             }
             //trigger change if not silent
@@ -60,11 +75,9 @@ define([
             var obj = {};
             for (var i in this._data) {
                 //if it's a submodel
-                if(this._data[i]
-                   && typeof this._data[i].getObject === 'function') {
+                if (this._data[i] && typeof this._data[i].getObject === 'function') {
                     obj[i] = this._data[i].getObject();
-                }
-                else {
+                } else {
                     obj[i] = this._data[i];
                 }
             }
