6ceedc35e5d3f67e13d9a0bfd3b5f0c15af081c9
Arthur Camara <arthurcamara@gmail.com>
2014-10-23 16:58:54 +0200

    Add one timeslider no model example

---
+++ src/tools/_examples-model-state/one-ts-no-model/one-ts-no-model.js
@@ -0,0 +1,31 @@
+//Example Timeslider A
+define([
+    'base/tool'
+], function(Tool) {
+
+
+    var OneTSNoModel = Tool.extend({
+        init: function(parent, options) {
+
+            //tool basic settings
+            this.name = 'one-ts-no-model';
+            this.template = 'tools/_examples-model-state/one-ts-no-model/one-ts-no-model';
+
+            //instantiating components
+            this.components = [{
+                component: '_gapminder/timeslider',
+                placeholder: '.vzb-tool-timeslider-1', //div to render
+            }];
+
+            this._super(parent, options);
+        },
+
+        validate: function() {
+
+        },
+
+
+    });
+
+    return OneTSNoModel;
+});
