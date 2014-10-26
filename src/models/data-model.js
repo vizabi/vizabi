092c6ccf2a2a0e78458f4f8350c686c6770cae6b
Arthur Camara <arthurcamara@gmail.com>
2014-10-26 03:39:38 +0100

    Validate data model

--- src/models/data-model.js
+++ src/models/data-model.js
@@ -12,16 +12,16 @@ define([
                 show: {},
                 selected: {},
                 source: {},
-                language: "en"
             }, values);
 
             this._super(values, interval);
             this._query = this._query || [];
+            this._language = this._language || "en";
             this.setSource();
 
             //reload data everytime parameter show, source or language changes
             var _this = this;
-            this.on(["change:show", "change:language"], function(evt) {
+            this.on(["change:show", "change:language", "change:query"], function(evt) {
                 _this.load();
             });
             //reload if source changes
@@ -38,8 +38,32 @@ define([
             this._dataManager = new DataManager(datapath);
         },
 
+        //validation
+        //todo: improve this validation
         validate: function() {
-            //validate
+            var indicator = this.get("show.indicator");
+            if (_.isArray(indicator) && indicator.length === 1) {
+                this.set("show.indicator", indicator[0]);
+            }
+            if (_.isArray(this._dataset) && this._dataset.length === 1) {
+                this._dataset = this._dataset[0];
+            }
+
+            var times = _.map(this._dataset, function(d) {
+                return parseInt(d.time,10);
+            });
+
+            if (times.length > 0) {
+                var min_time = _.min(times),
+                    max_time = _.max(times);
+                if (this.get("show.time_start") < min_time) {
+                    this.set("show.time_start", min_time);
+                }
+
+                if (this.get("show.time_end") > max_time) {
+                    this.set("show.time_end", max_time);
+                }
+            }
         },
 
         //overwrite get method
@@ -48,7 +72,24 @@ define([
             if (pars === "data") {
                 return this.getData();
             }
+            if (pars === "language") {
+                return this._language;
+            }
             return this._super(pars);
+        },
+
+        //overwrite set method
+        //it is done simply to make set("language", val) an alias
+        set: function(pars, value) {
+            if (pars === "query") {
+                this.setQuery(value);
+                this.trigger("change:query");
+            } else if (pars === "language") {
+                this.setLanguage(value);
+                this.trigger("change:language");
+            } else {
+                this._super(pars, value);
+            }
         },
 
         getData: function() {
@@ -59,12 +100,16 @@ define([
             this._query = query;
         },
 
+        setLanguage: function(lang) {
+            this._language = lang;
+        },
+
         //todo: remove deferred from this
         load: function() {
             var _this = this,
                 query = this._query,
-                language = this.get("language")
-                defer = $.Deferred();
+                language = this._language
+            defer = $.Deferred();
 
             this.trigger("load:start");
             //when request is completed, set it
@@ -77,6 +122,7 @@ define([
                         _this._dataset = _this._dataManager.get();
                         _this.validate();
                         _this.trigger("load:end");
+                        _this.trigger("change");
                         defer.resolve();
                     }
                 });
