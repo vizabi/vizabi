91578e439631f382aba824357c67b351264eff39
Arthur Camara <arthurcamara@gmail.com>
2014-10-30 13:59:19 +0100

    Add comments to timeslider

--- src/components/_gapminder/timeslider/timeslider.js
+++ src/components/_gapminder/timeslider/timeslider.js
@@ -8,8 +8,14 @@ define([
     var class_playing = "vzb-playing",
         class_hide_play = "vzb-ts-hide-play-button";
 
-
     var Timeslider = Component.extend({
+
+        /**
+         * Initializes the timeslider.
+         * Executed once before any template is rendered.
+         * @param options The options passed to the component
+         * @param context The component's parent
+         */
         init: function(options, context) {
             this.template = "components/_gapminder/timeslider/timeslider";
             if (!options.model) options.model = new TimeModel();
@@ -17,42 +23,41 @@ define([
             this._super(options, context);
         },
 
+        /**
+         * Executes after the template is loaded and rendered.
+         * Ideally, it contains HTML instantiations related to template
+         * At this point, this.element and this.placeholder are available as a d3 objects
+         */
         postRender: function() {
             var _this = this;
 
-            /* 
-             * html elements
-             */
+            //html elements
             this.range = this.element.select(".vzb-ts-slider");
             this.value = this.element.select('.vzb-ts-slider-value');
 
             var play = this.element.select(".vzb-ts-btn-play"),
                 pause = this.element.select(".vzb-ts-btn-pause");
 
-            /* 
-             * model and related events
-             */
-
+            //model and related events
             this.range.on('input', function() {
                 _this._setTime(parseFloat(this.value));
                 _this.model.pause();
             });
 
-            //play action
             play.on('click', function() {
                 _this.model.play();
             });
-            //pause action
+
             pause.on('click', function() {
                 _this.model.pause();
             });
         },
 
-
-        resize: function() {
-            //
-        },
-
+        /**
+         * Executes everytime there's an update event.
+         * Ideally, only operations related to changes in the model
+         * At this point, this.element is available as a d3 object
+         */
         update: function() {
             //time slider should always receive a time model
             var time = this.model.get("value"),
@@ -82,12 +87,19 @@ define([
             }
         },
 
+        /**
+         * Sets the time to current time model
+         * @param {number} time The time
+         * @param {boolean} silent Silent option does not trigger events
+         */
         _setTime: function(time, silent) {
             //update state
             this.model.set("value", time, silent);
-            this.update();
         },
 
+        /**
+         * Sets position of time value string
+         */
         _setTimePosition: function() {
             var rangeW = parseInt(this.range.style('width'),10) - 16,
                 timeRange = this.model.get("end") - this.model.get("start"),
