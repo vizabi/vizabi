f02973c4f1497d80c11b531ae9be7c1fda2bb1fa
Arthur Camara <arthurcamara@gmail.com>
2014-11-02 21:34:07 +0100

    Add comments to utils, remove unused functions and add isModel

--- src/base/utils.js
+++ src/base/utils.js
@@ -1,45 +1,67 @@
 define(['jquery', 'd3', 'underscore'], function($, d3, _) {
 
     var util = {
+        /**
+         * Converts a d3 selection to jQuery
+         * @param selector selection in d3
+         * @returns selection in jQuery
+         */
         d3ToJquery: function(selector) {
             return $(selector[0]);
         },
+        
+        /**
+         * Converts a jQuery selection to d3
+         * @param selector selection in jQuery
+         * @returns selection in d3
+         */
         jQueryToD3: function(selector) {
             return d3.selectAll(selector.toArray());
         },
+
+        /**
+         * Counts number of decimals in a number
+         * @example Example usage of countDecimals:
+         * // returns 2
+         * countDecimals(5.23);
+         * @param {Number} Number to be checked
+         * @returns {Number} Number of decimals.
+         */
         countDecimals: function(number) {
             if (Math.floor(number.valueOf()) === number.valueOf()) return 0;
             return number.toString().split(".")[1].length || 0;
         },
-        deepClone: function(obj, depth) {
-            if (typeof obj !== 'object') return obj;
-            if (_.isString(obj)) return obj.splice();
-            if (_.isDate(obj)) return new Date(obj.getTime());
-            if (_.isFunction(obj.clone)) return obj.clone();
-            var clone = _.isArray(obj) ? obj.slice() : _.extend({}, obj);
-            if (!_.isUndefined(depth) && (depth > 0)) {
-                for (var key in clone) {
-                    clone[key] = _.deepClone(clone[key], depth - 1);
-                }
-            }
-            return clone;
+
+        /**
+         * Checks if an object is a Vizabi model
+         * @param {Object} model object to be checked
+         * @returns {Boolean}
+         */
+        isModel: function(obj) {
+            return (obj._id && obj._id.indexOf("m") !== -1);
         },
-        deepExtend: function(target, src) {
-            for (var p in src) {
-                if (typeof src[p] === "object" &&
-                    src[p] !== null) {
-                    target[p] = target[p] || {};
-                    this.deepExtend(target[p], src[p]);
-                } else {
-                    target[p] = src[p];
-                }
-            }
-            return target;
-        },
+
+        /**
+         * Finds an intermediate value between two numbers
+         * @example Example usage of interpolate:
+         * // returns 120
+         * interpolate(100, 200, 0.20);
+         * @param {Number} value1 first number
+         * @param {Number} value2 second number
+         * @param {Number} fraction fraction to interpolate (0 <= fraction <= 1);
+         * @returns {Number}
+         */
         interpolate: function(value1, value2, fraction) {
             return value1 + ((value2 - value1) * fraction);
         },
 
+        /**
+         * Interpolates all values within a range. Same as d3.range
+         * @param {Number} val1 first number
+         * @param {Number} val2 second number
+         * @param {Number} step
+         * @returns {Array}
+         */
         interpolateRange: function(val1, val2, step) {
             return d3.range(val1, val2, step);
         }
