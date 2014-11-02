f80cdf57fafd1cd972cf002e115297c77e52c9d8
Arthur Camara <arthurcamara@gmail.com>
2014-11-02 23:03:37 +0100

    Event comments and ability to trigger parent events automatically

--- src/base/events.js
+++ src/base/events.js
@@ -1,20 +1,31 @@
 define([
-    "underscore",
+    "lodash",
     "base/class"
 ], function(_, Class) {
 
     var Events = Class.extend({
 
+        /**
+         * Initializes the event class
+         */
         init: function() {
             this.events = {};
         },
 
-        //alias to bind
+        /**
+         * Alias to bind (@see: bind)
+         * @param {String|Array} name name of event or array with names
+         * @param {Function} func function to be linked with event
+         */
         on: function(name, func) {
             this.bind(name, func);
         },
 
-        //bind a function to a certain event
+        /**
+         * Binds a callback function to an event
+         * @param {String|Array} name name of event or array with names
+         * @param {Function} func function to be linked with event
+         */
         bind: function(name, func) {
 
             //bind multiple at a time
@@ -35,6 +46,12 @@ define([
                 console.log(func);
             }
         },
+
+        /**
+         * Unbinds all events associated with a name or a specific one
+         * @param {String|Array} name name of event or array with names
+         * @param {Function} func function to be removed (if provided)
+         */
         unbind: function(name, func) {
 
             //unbind multiple at a time
@@ -51,25 +68,34 @@ define([
                 this.events[name] = [];
             }
         },
+
+        /**
+         * Unbinds all events
+         */
         unbindAll: function() {
             this.events = {};
         },
 
-        //trigger event and all parents. E.g: change:language and change
-        trigger: function(name, args) {
+        /**
+         * Triggers an event
+         * @param {String|Array} name name of event or array with names
+         * @param args Optional arguments (values to be passed)
+         */
+        trigger: function(name, args, original) {
             if (_.isArray(name)) {
                 for (var i = 0, size = name.length; i < size; i++) {
                     this.trigger(name[i], args);
                 }
             } else {
+                console.log('triggered => name: ' + name + ' , original: ' + original);
                 if (_.isUndefined(this.events[name])) return;
                 for (var i = 0; i < this.events[name].length; i++) {
                     var f = this.events[name][i];
                     if (_.isFunction(f)) {
                         if (_.isUndefined(args)) {
-                            f(name);
+                            f((original || name));
                         } else {
-                            f(name, args);
+                            f((original || name), args);
                         }
                     } else {
                         console.log("Can't execute event '" + name + ". The following must be a function: ");
@@ -77,8 +103,29 @@ define([
                     }
                 }
             }
+        },
 
-        }
+        /**
+         * Triggers an and all parent events
+         * @param {String|Array} name name of event or array with names
+         * @param args Optional arguments (values to be passed)
+         */
+        triggerAll: function(name, args) {
+            if (_.isArray(name)) {
+                for (var i = 0, size = name.length; i < size; i++) {
+                    this.triggerAll(name[i], args);
+                }
+            } else {
+                var original = name,
+                    parts = name.split(":");
+                while (parts.length) {
+                    this.trigger(name, args, original);
+                    parts.pop();
+                    name = parts.join(":");
+                }
+            }
+        },
+
     });
 
     return Events;
