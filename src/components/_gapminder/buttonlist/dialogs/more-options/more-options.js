8b6d083fcc163f7ac9e240d03c1ef718d2db6b8b
Arthur Camara <arthurcamara@gmail.com>
2014-12-03 16:43:53 +0100

    Change "More options" example to include only a long text

--- src/components/_gapminder/buttonlist/dialogs/more-options/more-options.js
+++ src/components/_gapminder/buttonlist/dialogs/more-options/more-options.js
@@ -11,48 +11,6 @@ define([
          */
         init: function(config, parent) {
             this.name = 'more-options';
-
-            this.components = [{
-                component: '_gapminder/timeslider',
-                placeholder: '.vzb-dialog-timeslider',
-                model: ["state.time"],
-                ui: {
-                    show_button: false,
-                    show_value: true
-                }
-            }, {
-                component: '_gapminder/timeslider',
-                placeholder: '.vzb-dialog-timeslider-2',
-                model: ["state.time"],
-                ui: {
-                    show_button: true
-                }
-            }, {
-                component: '_gapminder/timeslider',
-                placeholder: '.vzb-dialog-timeslider-3',
-                model: ["state.time"],
-                ui: {
-                    show_button: false,
-                    show_limits: true
-                }
-            }, {
-                component: '_gapminder/timeslider',
-                placeholder: '.vzb-dialog-timeslider-4',
-                model: ["state.time"],
-                ui: {
-                    show_button: true,
-                    show_value: true
-                }
-            }, {
-                component: '_gapminder/timeslider',
-                placeholder: '.vzb-dialog-timeslider-5',
-                model: ["state.time"],
-                ui: {
-                    show_button: false,
-                    show_limits: true
-                }
-            }];
-
             this._super(config, parent);
         }
     });
