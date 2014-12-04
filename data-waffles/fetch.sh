10b4ef99c50446dbce35443f0c543ea309938195
Arthur Camara <arthurcamara@gmail.com>
2014-12-04 13:39:12 +0100

    Update fetch script for data

--- data-waffles/fetch.sh
+++ data-waffles/fetch.sh
@@ -11,9 +11,9 @@ languages=( en fa pt )
 for i in "${languages[@]}"
 do
 echo "Fetching data for language: "$i
-curl -s -H "Content-Type: application/json" -d '{"query":[{"SELECT":["geo","geo.name","time","geo.region", "geo.category","lex","gdp_per_cap","pop"],"WHERE":{"geo":["*"],"geo.category":["*"],"time":["1990-2014"]}}],"language":"'$i'"}'  http://waffle-server-1.herokuapp.com/api/query > examples/$i/response_0.json
+curl -s -H "Content-Type: application/json" -d '{"query":[{"SELECT":["geo","geo.name","time","geo.region", "geo.category","lex","gdp_per_cap","pop"],"WHERE":{"geo":["*"],"geo.category":["*"],"time":["1990-2014"]}}],"language":"'$i'"}'  http://waffle-server-1.herokuapp.com/api/query > examples/$i/basic-indicators.json
 
-curl -s -H "Content-Type: application/json" -d '{"query":[{"SELECT":["geo","geo.name", "geo.region", "geo.category"],"WHERE":{"geo":["*"],"geo.category":["*"]}}],"language":"'$i'"}'  http://waffle-server-1.herokuapp.com/api/query > examples/$i/response_1.json
+curl -s -H "Content-Type: application/json" -d '{"query":[{"SELECT":["geo","geo.name", "geo.region", "geo.category"],"WHERE":{"geo":["*"],"geo.category":["*"]}}],"language":"'$i'"}'  http://waffle-server-1.herokuapp.com/api/query > examples/$i/names.json
 echo "SAVED!"
 done
 
