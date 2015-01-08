93dc80db0ce8a724a5e7354b1f021fadf8b6ef31
ambodi <amir.rahnama@gmail.com>
2015-01-08 11:52:20 +0100

    update the line chart tool: remove query, add model_expects

--- src/tools/_gapminder/line-chart/line-chart.js
+++ src/tools/_gapminder/line-chart/line-chart.js
@@ -13,22 +13,22 @@ define([
             this.name = 'line-chart';
             this.template = "tools/_gapminder/line-chart/line-chart";
 
-            this.state = options.state;
-
             this.components = [{
                 component: '_gapminder/header',
                 placeholder: '.vzb-tool-title'
             }, {
                 component: '_gapminder/line-chart',
                 placeholder: '.vzb-tool-viz',
-                model: ["state.show", "data", "state.time"]
+                model: ["state.time", "state.entities", "state.marker", "data"]
             }, {
                 component: '_gapminder/timeslider',
                 placeholder: '.vzb-tool-timeslider',
                 model: ["state.time"]
             },{
                 component: '_gapminder/buttonlist',
-                placeholder: '.vzb-tool-buttonlist'
+                placeholder: '.vzb-tool-buttonlist',
+                model: ['state', 'data', 'language'],
+                buttons: ['colors', 'size', 'more-options']
             }];
 
             this._super(config, options);
@@ -39,43 +39,23 @@ define([
          * @param model the current tool model to be validated
          */
         toolModelValidation: function(model) {
-
-            var state = model.state;
-            var data = model.data;
+            var time = model.state.time,
+                markers = model.state.marker.label;
 
             //don't validate anything if data hasn't been loaded
-            if(!data.getItems() || data.getItems().length < 1) {
+            if (!markers.getItems() || markers.getItems().length < 1) {
                 return;
             }
 
-            var dateMin = data.getLimits('time').min,
-                dateMax = data.getLimits('time').max;
+            var dateMin = markers.getLimits('time').min,
+                dateMax = markers.getLimits('time').max;
 
-            if (state.time.start < dateMin) {
-                state.time.start = dateMin;
+            if (time.start < dateMin) {
+                time.start = dateMin;
             }
-            if (state.time.end > dateMax) {
-                state.time.end = dateMax;
+            if (time.end > dateMax) {
+                time.end = dateMax;
             }
-        },
-
-        /**
-         * Returns the query (or queries) to be performed by this tool
-         * @param model the tool model will be received
-         */
-        getQuery: function(model) {
-            var state = model.state,
-                time_start = d3.time.format("%Y")(state.time.start),
-                time_end = d3.time.format("%Y")(state.time.end);
-            return [{
-                "from": "data",
-                "select": ["geo", "geo.name", "time", "geo.region", "geo.category", state.show.indicator],
-                "where": {
-                    "geo": state.show.geo,
-                    "geo.category": state.show.geo_category,
-                    "time": [time_start + "-" + time_end]
-                }
-            }];
         }
     });
 
