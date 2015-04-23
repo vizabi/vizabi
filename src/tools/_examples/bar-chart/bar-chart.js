f57dc10b9f0c1bf766005422c6968f6a282bc4f2
Arthur Camara <arthurcamara@gmail.com>
2015-04-23 07:59:09 +0200

    Add first version of default states to bar chart

--- src/tools/_examples/bar-chart/bar-chart.js
+++ src/tools/_examples/bar-chart/bar-chart.js
@@ -12,28 +12,220 @@ define([
          * @param {Object} options Options such as state, data, etc
          */
         init: function(config, options) {
-            
-            this.name = "bar-chart";
-            this.template = "tools/_examples/"+this.name+"/"+this.name;
 
-	        //specifying components
+            this.name = "bar-chart";
+            this.template = "tools/_examples/" + this.name + "/" + this.name;
+
+            //specifying components
             this.components = [{
                 component: '_examples/bar-chart',
                 placeholder: '.vzb-tool-viz',
-                model: ["state.time", "state.entities", "state.marker", "language"]  //pass models to component
-            },
-            {
+                model: ["state.time", "state.entities", "state.marker", "language"] //pass models to component
+            }, {
                 component: '_gapminder/buttonlist',
                 placeholder: '.vzb-tool-buttonlist',
                 model: ['state', 'ui', 'language'],
                 buttons: ['fullscreen', 'find', 'colors']
-            },
-            {
+            }, {
                 component: '_gapminder/timeslider',
                 placeholder: '.vzb-tool-timeslider',
                 model: ["state.time"]
-            },
-            ];
+            }, ];
+
+            //default options
+            this.default_options = {
+                state: {
+                    _type_: "model",
+                    _defs_: {
+                        //timespan of the visualization
+                        time: {
+                            _type_: "model",
+                            _defs_: {
+                                start: 1952,
+                                end: 2012,
+                                value: 2000,
+                                step: 1,
+                                speed: 300,
+                                formatInput: "%Y"
+                            }
+                        },
+                        //entities we want to show
+                        entities: {
+                            _type_: "model",
+                            _defs_: {
+                                show: {
+                                    _type_: "model",
+                                    _defs_: {
+                                        dim: {
+                                            _type_: "string",
+                                            _defs_: "geo",
+                                        },
+                                        filter: {
+                                            _type_: "object",
+                                            _defs_: {
+                                                "geo": ["*"],
+                                                "geo.category": ["region"]
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        },
+
+                        //how we show it
+                        marker: {
+                            _type_: "model",
+                            _defs_: {
+                                dimensions: {
+                                    _type_: "array",
+                                    _defs_: ["entities", "time"]
+                                },
+                                type: "geometry",
+                                shape: "circle",
+                                label: {
+                                    _type_: "hook",
+                                    _defs_: {
+                                        use: {
+                                            _type_: "string",
+                                            _defs_: "property",
+                                            _opts_: ["property", "indicator", "value"]
+                                        },
+                                        value: {
+                                            _type_: "string",
+                                            _defs_: "geo.name"
+                                        }
+                                    }
+                                },
+                                axis_y: {
+                                    _type_: "hook",
+                                    _defs_: {
+                                        use: {
+                                            _type_: "string",
+                                            _defs_: "indicator"
+                                        },
+                                        value: {
+                                            _type_: "string",
+                                            _defs_: "lex"
+                                        },
+                                        scale: {
+                                            _type_: "string",
+                                            _defs_: "linear"
+                                        }
+                                    }
+                                },
+                                axis_x: {
+                                    _type_: "hook",
+                                    _defs_: {
+                                        use: {
+                                            _type_: "string",
+                                            _defs_: "property",
+                                            _opts_: ["property"]
+                                        },
+                                        value: {
+                                            _type_: "string",
+                                            _defs_: "geo.name"
+                                        },
+                                    }
+                                },
+                                color: {
+                                    _type_: "hook",
+                                    _defs_: {
+                                        use: {
+                                            _type_: "string",
+                                            _defs_: "property"
+                                        },
+                                        value: {
+                                            _type_: "string",
+                                            _defs_: "geo.region"
+                                        },
+                                        domain: {
+                                            _type_: "object",
+                                            _defs_: {
+                                                "_default": "#ffb600",
+                                                "world": "#ffb600",
+                                                "eur": "#FFE700",
+                                                "afr": "#00D5E9",
+                                                "asi": "#FF5872",
+                                                "ame": "#7FEB00"
+                                            }
+                                        },
+                                        domain_type: {
+                                            _type_: "string",
+                                            _defs_: "continuous"
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                },
+
+                data: {
+                    _type_: "model",
+                    _defs_: {
+                        reader: {
+                            _type_: "string",
+                            _defs_: "waffle-server"
+                        }
+                    }
+                },
+
+                ui: {
+                    _type_: "model",
+                    _defs_: {
+                        'vzb-tool-bar-chart': {
+                            _type_: "object",
+                            _defs_: {
+                                whenHovering: {
+                                    showProjectionLineX: true,
+                                    showProjectionLineY: true,
+                                    higlightValueX: true,
+                                    higlightValueY: true
+                                },
+                                labels: {
+                                    autoResolveCollisions: true,
+                                    dragging: true
+                                }
+                            }
+                        }
+                    }
+                },
+
+                //language properties
+                language: {
+                    _type_: "model",
+                    _defs_: {
+                        id: {
+                            _type_: "string",
+                            _defs_: "en"
+                        },
+                        strings: {
+                            _type_: "object",
+                            _defs_: {
+                                en: {
+                                    "title": "Bubble Chart Title",
+                                    "buttons/expand": "Full screen",
+                                    "buttons/unexpand": "Leave full screen",
+                                    "buttons/trails": "Trails",
+                                    "buttons/lock": "Lock",
+                                    "buttons/find": "Find",
+                                    "buttons/colors": "Colors",
+                                    "buttons/size": "Size",
+                                    "buttons/more_options": "Options",
+                                    "indicator/lex": "Life expectancy",
+                                    "indicator/gdp_per_cap": "GDP per capita",
+                                    "indicator/pop": "Population",
+                                    "indicator/geo.region": "Region",
+                                    "indicator/geo": "Geo code",
+                                    "indicator/time": "",
+                                    "indicator/geo.category": "Geo category"
+                                }
+                            }
+                        }
+                    }
+                }
+            };
 
             //constructor is the same as any tool
             this._super(config, options);
