d72de2378f1149198f06537f19b1c98c2b7a1f55
Angie Skazka <konzeptmeister@gmail.com>
2015-05-10 12:05:09 +0200

    Add color wheel

--- src/components/_gapminder/color-legend/color-legend.js
+++ src/components/_gapminder/color-legend/color-legend.js
@@ -58,20 +58,114 @@ define([
             this._super(config, context);
         },
 
-        /**
-         * Executes after the template is loaded and rendered.
-         * Ideally, it contains HTML instantiations related to template
-         * At this point, this.element and this.placeholder are available as a d3 object
-         */
+
         domReady: function() {
+
+            
+            var _this = this;
+            
+            this.listColors = this.element.append("div").attr("class", "vzb-cl-colorList");
+            
+            var nHues = 20;
+            var nLightness = 6;
+            var minRadius = 15;
+            var minLightness = 50;
+            var minHue = 15;
+            var saturation = "70%";
+            colorData = [];
+            for(var j = 0; j<nLightness; j++) {
+                var lightness = (minLightness+(100-minLightness)/nLightness * j) + "%";
+
+                colorData.push([]);
+                for(var i = 0; i<=nHues; i++) {
+                    var hue = minHue+(364-minHue)/nHues * i;
+                    colorData[j].push("hsl(" + hue + "," + saturation + "," + lightness +  ")");
+                }
+            }
+
+            this.pickerCanvas = this.element.append("svg")
+                .attr("class", "vzb-cl-colorPicker vzb-invisible");
+            
+            var width = parseInt(this.pickerCanvas.style("width"));
+            var height = parseInt(this.pickerCanvas.style("height"));
+            var radius = width/2*0.8;
+
+            
+            this.pickerCanvas.append("rect")
+                .attr("width", width)
+                .attr("height", height)
+                .on("mousemove", function(){
+                    _this.chosenColor = _this.currentColor;
+                    _this.colorSample.style("fill", _this.chosenColor);
+                })
+                .on("mouseout", function(){
+                })
+                .on("click", function(){
+                    _this.pickerCanvas.classed("vzb-invisible", true);
+                    _this.listColors.classed("vzb-invisible", false);
+                });
+            
+            this.pickerCanvas.append("rect")
+                .attr("class", "vzb-cl-colorSample")
+                .attr("width", width/2)
+                .attr("height", 10);
+            this.colorSample = this.pickerCanvas.append("rect")
+                .attr("class", "vzb-cl-colorSample")
+                .attr("width", width/2)
+                .attr("x", width/2)
+                .attr("height", 10);
+            
+            var arc = d3.svg.arc();
+
+            var pie = d3.layout.pie()
+                .sort(null)
+                .value(function(d) { return 1 });
+            
+            this.pickerCanvas.selectAll(".circle")
+                .data(colorData)
+                .enter().append("g")
+                .attr("class", "circle")
+                .attr("transform", "translate("+(radius + width*0.1) +","+(radius + width*0.1) +")")
+                .each(function(circleData, index){
+                    
+                    arc.outerRadius(minRadius+(radius-minRadius)/nLightness*(nLightness-index))
+                        .innerRadius(minRadius+(radius-minRadius)/nLightness*(nLightness-index-1));
+                    
+                    var segment = d3.select(this).selectAll(".segment")
+                        .data(pie(circleData))
+                        .enter().append("g")
+                        .attr("class", "segment");
+                
+                    segment.append("path")
+                        .attr("d", arc)
+                        .style("fill", function(d) {return d.data })
+                        .on("mousemove", function(){
+                            d3.select(this).style("stroke", "white");
+                            _this.chosenColor = d3.select(this).style("fill");
+                            _this.colorSample.style("fill", _this.chosenColor);
+                            _this._setModel(_this.chosenColor);
+                        })
+                        .on("mouseout", function(){
+                            d3.select(this).style("stroke", "none");
+                        })
+                        .on("click", function(){
+                            d3.select(this).style("stroke", "none");
+                            _this.pickerCanvas.classed("vzb-invisible", true);
+                            _this.listColors.classed("vzb-invisible", false);
+                            
+                        });
+                })
+                
+            
         },
         
         updateView: function(){
+            var _this = this;
             this.translator = this.model.language.getTFunction();
             var indicator = this.model.color[INDICATOR];
             var data = Object.keys(availOpts[indicator]);
 
-            var el_colors = this.element
+            var el_colors = this.listColors
                 .selectAll(".vzb-cl-option")
                 .data(data, function(d){return d});
 
@@ -81,19 +175,41 @@ define([
                 .each(function(d){
                     d3.select(this).append("div")
                         .attr("class", "vzb-cl-color-sample")
-                        .style("background-color",availOpts[indicator][d]);
+                        .style("background-color",availOpts[indicator][d])
+                        .style("border", "1px solid " + availOpts[indicator][d]);
                 
                     d3.select(this).append("div")
                         .attr("class", "vzb-cl-color-legend")
                         .text(d);
-                });
+                })
+                .on("mousemove", function(d){
+                    var sample = d3.select(this).select(".vzb-cl-color-sample");
+                    sample.style("border-width", "5px");
+                    sample.style("background-color", "transparent");
 
+                })
+                .on("mouseout", function(d){
+                    var sample = d3.select(this).select(".vzb-cl-color-sample");
+                    sample.style("border-width", "0px");
+                    sample.style("background-color", availOpts[indicator][d]);
+                })
+                .on("click", function(d){
+                    _this.pickerCanvas.classed("vzb-invisible", false);
+                    _this.listColors.classed("vzb-invisible", true);
+                    _this.currentColor = availOpts[indicator][d];
+                    _this.pickerCanvas.selectAll(".vzb-cl-colorSample")
+                        .style("fill", _this.currentColor);
+                    _this.target = {indicator: indicator, segment: d};
+                });
         },
         
         
         
-        _setModel: function (what, value) {
-            this.model.color.domain = this.domain;
+        _setModel: function (value) {
+            var _this = this;
+            availOpts[_this.target.indicator][_this.target.segment] = value;
+            
+            this.model.color.domain = availOpts[_this.target.indicator];
         }
         
 
