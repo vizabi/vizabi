8c8990cff743ba6fc5f4916042be93523dc8a840
Angie Skazka <konzeptmeister@gmail.com>
2015-05-10 13:29:54 +0200

    Improve performance

--- src/components/_gapminder/color-legend/color-legend.js
+++ src/components/_gapminder/color-legend/color-legend.js
@@ -18,6 +18,33 @@ define([
         '42':           '#fa5ed6'
     };
     
+    
+    function _hslToRgb(h, s, l){
+        var r, g, b;
+
+        if(s == 0){
+            r = g = b = l; // achromatic
+        }else{
+            var hue2rgb = function hue2rgb(p, q, t){
+                if(t < 0) t += 1;
+                if(t > 1) t -= 1;
+                if(t < 1/6) return p + (q - p) * 6 * t;
+                if(t < 1/2) return q;
+                if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+                return p;
+            }
+
+            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+            var p = 2 * l - q;
+            r = hue2rgb(p, q, h + 1/3);
+            g = hue2rgb(p, q, h);
+            b = hue2rgb(p, q, h - 1/3);
+        }
+
+        return "#" + Math.round(r * 255).toString(16) + Math.round(g * 255).toString(16) + Math.round(b * 255).toString(16);
+    }
+    
+    
 
     var ColorLegend = Component.extend({
 
@@ -69,17 +96,17 @@ define([
             var nHues = 20;
             var nLightness = 6;
             var minRadius = 15;
-            var minLightness = 50;
-            var minHue = 15;
-            var saturation = "70%";
+            var minLightness = 0.50;
+            var minHue = 0.05;
+            var saturation = 0.7;
             colorData = [];
             for(var j = 0; j<nLightness; j++) {
-                var lightness = (minLightness+(100-minLightness)/nLightness * j) + "%";
+                var lightness = (minLightness+(1-minLightness)/nLightness * j);
 
                 colorData.push([]);
                 for(var i = 0; i<=nHues; i++) {
-                    var hue = minHue+(364-minHue)/nHues * i;
-                    colorData[j].push("hsl(" + hue + "," + saturation + "," + lightness +  ")");
+                    var hue = minHue+(1-minHue)/nHues * i;
+                    colorData[j].push(_hslToRgb(hue, saturation, lightness));
                 }
             }
 
@@ -94,9 +121,10 @@ define([
             this.pickerCanvas.append("rect")
                 .attr("width", width)
                 .attr("height", height)
-                .on("mousemove", function(){
+                .on("mouseover", function(){
                     _this.chosenColor = _this.currentColor;
                     _this.colorSample.style("fill", _this.chosenColor);
+                    _this._setModel(_this.chosenColor);
                 })
                 .on("mouseout", function(){
                 })
@@ -139,7 +167,8 @@ define([
                     segment.append("path")
                         .attr("d", arc)
                         .style("fill", function(d) {return d.data })
-                        .on("mousemove", function(){
+                        .on("mouseover", function(){
+                        
                             d3.select(this).style("stroke", "white");
                             _this.chosenColor = d3.select(this).style("fill");
                             _this.colorSample.style("fill", _this.chosenColor);
