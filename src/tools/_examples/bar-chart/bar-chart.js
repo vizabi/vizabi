d35550416f1cbf24a6619f1c0720e92df5610f6d
Arthur Camara <arthurcamara@gmail.com>
2015-05-25 15:15:52 +0200

    Bar chart example tool and component

--- src/tools/_examples/bar-chart/bar-chart.js
+++ src/tools/_examples/bar-chart/bar-chart.js
@@ -16,6 +16,266 @@
         return;
     }
 
+    var comp_template = 'src/tools/_examples/bar-chart/bar-chart-comp.html';
+
+    //BAR CHART COMPONENT
+    Vizabi.Component.extend('gapminder-barchart', {
+
+        /**
+         * Initializes the component (Bar Chart).
+         * Executed once before any template is rendered.
+         * @param {Object} config The options passed to the component
+         * @param {Object} context The component's parent
+         */
+        init: function(config, context) {
+            this.name = 'bar-chart';
+            this.template = comp_template;
+
+            //define expected models for this component
+            this.model_expects = [{
+                name: "time",
+                type: "time"
+            }, {
+                name: "entities",
+                type: "entities"
+            }, {
+                name: "marker",
+                type: "model"
+            }, {
+                name: "language",
+                type: "language"
+            }];
+
+            var _this = this;
+            this.model_binds = {
+                "change:time:value": function(evt) {
+                    _this.updateEntities();
+                }
+            };
+
+            //contructor is the same as any component
+            this._super(config, context);
+
+            this.xScale = null;
+            this.yScale = null;
+            this.cScale = d3.scale.category10();
+
+            this.xAxis = d3.svg.axisSmart();
+            this.yAxis = d3.svg.axisSmart();
+        },
+
+        /**
+         * DOM is ready
+         */
+        domReady: function() {
+
+            this.element = d3.select(this.element);
+
+            this.graph = this.element.select('.vzb-bc-graph');
+            this.yAxisEl = this.graph.select('.vzb-bc-axis-y');
+            this.xAxisEl = this.graph.select('.vzb-bc-axis-x');
+            this.yTitleEl = this.graph.select('.vzb-bc-axis-y-title');
+            this.bars = this.graph.select('.vzb-bc-bars');
+
+            var _this = this;
+            this.on("resize", function() {
+                _this.updateEntities();
+            });
+        },
+
+        /*
+         * Both model and DOM are ready
+         */
+        ready: function() {
+            this.updateIndicators();
+            this.resize();
+            this.updateEntities();
+        },
+
+        /**
+         * Changes labels for indicators
+         */
+        updateIndicators: function() {
+            var _this = this;
+            this.translator = this.model.language.getTFunction();
+            this.duration = this.model.time.speed;
+            this.timeFormatter = d3.time.format(this.model.time.formatInput);
+
+            var titleStringY = this.translator("indicator/" + this.model.marker.axis_y.value);
+
+            var yTitle = this.yTitleEl.selectAll("text").data([0]);
+            yTitle.enter().append("text");
+            yTitle
+                .attr("y", "-6px")
+                .attr("x", "-9px")
+                .attr("dx", "-0.72em")
+                .text(titleStringY);
+
+            this.yScale = this.model.marker.axis_y.getScale();
+            this.xScale = this.model.marker.axis_x.getScale();
+            this.cScale = this.model.marker.color.getScale();
+
+            this.yAxis.tickFormat(_this.model.marker.axis_y.tickFormatter);
+            this.xAxis.tickFormat(_this.model.marker.axis_x.tickFormatter);
+        },
+
+        /**
+         * Updates entities
+         */
+        updateEntities: function() {
+
+            var _this = this;
+            var time = this.model.time;
+            var currTime = time.value;
+            var duration = (time.playing) ? time.speed : 0;
+
+            var items = this.model.marker.label.getItems({
+                time: currTime
+            });
+
+            this.entityBars = this.bars.selectAll('.vzb-bc-bar')
+                .data(items);
+
+            //exit selection
+            this.entityBars.exit().remove();
+
+            //enter selection -- init circles
+            this.entityBars.enter().append("rect")
+                .attr("class", "vzb-bc-bar")
+                .on("mousemove", function(d, i) {
+                })
+                .on("mouseout", function(d, i) {
+                })
+                .on("click", function(d, i) {
+                });
+
+            //positioning and sizes of the bars
+
+            var bars = this.bars.selectAll('.vzb-bc-bar');
+
+            barWidth = this.xScale.rangeBand();
+
+            this.bars.selectAll('.vzb-bc-bar')
+                .attr("width", barWidth)
+                .attr("fill", function(d) {
+                    return _this.cScale(_this.model.marker.color.getValue(d));
+                })
+                .attr("x", function(d) {
+                    return _this.xScale(_this.model.marker.axis_x.getValue(d));
+                })
+                .transition().duration(duration).ease("linear")
+                .attr("y", function(d) {
+                    return _this.yScale(_this.model.marker.axis_y.getValue(d));
+                })
+                .attr("height", function(d) {
+                    return _this.height - _this.yScale(_this.model.marker.axis_y.getValue(d));
+                });
+        },
+
+        /**
+         * Executes everytime the container or vizabi is resized
+         * Ideally,it contains only operations related to size
+         */
+        resize: function() {
+
+            var _this = this;
+
+            this.profiles = {
+                "small": {
+                    margin: {
+                        top: 30,
+                        right: 20,
+                        left: 40,
+                        bottom: 40
+                    },
+                    padding: 2,
+                    minRadius: 2,
+                    maxRadius: 40
+                },
+                "medium": {
+                    margin: {
+                        top: 30,
+                        right: 60,
+                        left: 60,
+                        bottom: 40
+                    },
+                    padding: 2,
+                    minRadius: 3,
+                    maxRadius: 60
+                },
+                "large": {
+                    margin: {
+                        top: 30,
+                        right: 60,
+                        left: 60,
+                        bottom: 40
+                    },
+                    padding: 2,
+                    minRadius: 4,
+                    maxRadius: 80
+                }
+            };
+
+            this.activeProfile = this.profiles[this.getLayoutProfile()];
+            var margin = this.activeProfile.margin;
+
+
+            //stage
+            this.height = parseInt(this.element.style("height"), 10) - margin.top - margin.bottom;
+            this.width = parseInt(this.element.style("width"), 10) - margin.left - margin.right;
+
+            this.graph
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            //update scales to the new range
+            if (this.model.marker.axis_y.scaleType !== "ordinal") {
+                this.yScale.range([this.height, 0]);
+            } else {
+                this.yScale.rangePoints([this.height, 0], _this.activeProfile.padding).range();
+            }
+            if (this.model.marker.axis_x.scaleType !== "ordinal") {
+                this.xScale.range([0, this.width]);
+            } else {
+                this.xScale.rangePoints([0, this.width], _this.activeProfile.padding).range();
+            }
+
+            //apply scales to axes and redraw
+            this.yAxis.scale(this.yScale)
+                .orient("left")
+                .tickSize(6, 0)
+                .tickSizeMinor(3, 0)
+                .labelerOptions({
+                    scaleType: this.model.marker.axis_y.scaleType,
+                    toolMargin: margin,
+                    limitMaxTickNumber: 6
+                });
+
+            this.xAxis.scale(this.xScale)
+                .orient("bottom")
+                .tickSize(6, 0)
+                .tickSizeMinor(3, 0)
+                .labelerOptions({
+                    scaleType: this.model.marker.axis_x.scaleType,
+                    toolMargin: margin
+                });
+
+            this.xAxisEl.attr("transform", "translate(0," + this.height + ")")
+                .call(this.xAxis);
+
+            this.xScale.rangeRoundBands([0, this.width], 0.1, 0.2);
+
+            this.yAxisEl.call(this.yAxis);
+            this.xAxisEl.call(this.xAxis);
+
+        },
+
+
+        drawBars: function() {
+
+        }
+    });
+
+    //BAR CHART TOOL
     Vizabi.Tool.extend('BarChart', {
 
         /**
@@ -30,19 +290,22 @@
 
             //specifying components
             this.components = [{
-                component: '_examples/bar-chart',
-                placeholder: '.vzb-tool-viz',
-                model: ["state.time", "state.entities", "state.marker", "language"] //pass models to component
-            }, {
-                component: '_gapminder/buttonlist',
-                placeholder: '.vzb-tool-buttonlist',
-                model: ['state', 'ui', 'language'],
-                buttons: ['fullscreen']
-            }, {
-                component: '_gapminder/timeslider',
-                placeholder: '.vzb-tool-timeslider',
-                model: ["state.time"]
-            }];
+                    component: 'gapminder-barchart',
+                    placeholder: '.vzb-tool-viz',
+                    model: ["state.time", "state.entities", "state.marker", "language"] //pass models to component
+                },
+                // {
+                //     component: '_gapminder/buttonlist',
+                //     placeholder: '.vzb-tool-buttonlist',
+                //     model: ['state', 'ui', 'language'],
+                //     buttons: ['fullscreen']
+                // },
+                {
+                    component: 'gapminder-timeslider',
+                    placeholder: '.vzb-tool-timeslider',
+                    model: ["state.time"]
+                }
+            ];
 
             //default options
             this.default_options = {
@@ -172,7 +435,11 @@
                     _defs_: {
                         reader: {
                             _type_: "string",
-                            _defs_: "waffle-server"
+                            _defs_: "local-json"
+                        },
+                        path: {
+                            _type_: "string",
+                            _defs_: "local_data/waffles/{{LANGUAGE}}/basic-indicators.json"
                         }
                     }
                 },
@@ -252,5 +519,6 @@
             }
         }
     });
+
 
 }).call(this);
