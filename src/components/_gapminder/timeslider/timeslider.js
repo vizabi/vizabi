e8a414990c26f6ac2123d686bdae748a6076befe
Angie Skazka <konzeptmeister@gmail.com>
2015-08-03 21:45:22 +0200

    Fixed timeslider initialisation bug which only appears on a slow connection

--- src/components/_gapminder/timeslider/timeslider.js
+++ src/components/_gapminder/timeslider/timeslider.js
@@ -317,6 +317,8 @@
 
       var old_pos = this.handle.attr("cx");
       var new_pos = this.xScale(value);
+        
+      if(old_pos==null) old_pos = new_pos;
       var speed = new_pos > old_pos ? this.model.time.speed : 0;
 
 
