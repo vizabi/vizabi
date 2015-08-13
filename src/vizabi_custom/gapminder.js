72eb9d40b9f3f6ccbf3672e248bfbf9d2b39ea9f
Angie Skazka <konzeptmeister@gmail.com>
2015-08-13 18:57:23 +0200

    Updated link to Ola's dropbox

--- src/vizabi_custom/gapminder.js
+++ src/vizabi_custom/gapminder.js
@@ -276,7 +276,7 @@
             reader: "csv-file",
             //path: "local_data/waffles/{{LANGUAGE}}/mountains-pop-gdp-gini.csv"
             //path: "local_data/waffles/{{LANGUAGE}}/mountains-pop-gdp-gini-1800-2030.csv"
-            path: "https://dl.dropboxusercontent.com/u/91563274/mountains-pop-gdp-gini-1800-2030.csv"
+            path: "https://dl.dropboxusercontent.com/u/21736853/data/process/inc_mount_data_2015test/mountains-pop-gdp-gini-1800-2030.csv"
         }
     });
 
