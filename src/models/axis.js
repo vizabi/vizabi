c37a7b67739669b6273f5eebb45d441f7fe5b093
Arthur Camara <arthurcamara@gmail.com>
2015-08-20 15:33:22 +0200

    Allow min to be less than max. Fixes #354

--- src/models/axis.js
+++ src/models/axis.js
@@ -71,7 +71,9 @@
 
           if(this.min<=0 && this.scaleType=="log") this.min = 0.01;
           if(this.max<=0 && this.scaleType=="log") this.max = 10;
-          if(this.min>=this.max) this.min = this.max/2;
+
+          // Max may be less than min
+          // if(this.min>=this.max) this.min = this.max/2;
 
           if(this.min!=this.scale.domain()[0] || this.max!=this.scale.domain()[1])
               this.scale.domain([this.min, this.max]);
