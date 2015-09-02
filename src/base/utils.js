c26442e36ba79b42cac8759e702b16f3dcd798ab
IncoCode <akulich.dimon@gmail.com>
2015-09-02 11:38:43 +0300

    Add debounce function in utils

--- src/base/utils.js
+++ src/base/utils.js
@@ -821,6 +821,25 @@
 
         return (hash in fn.memoize) ? fn.memoize[hash] : fn.memoize[hash] = fn.apply(this, args);
       };
+    },
+
+    // Returns a function, that, as long as it continues to be invoked, will not
+    // be triggered. The function will be called after it stops being called for
+    // N milliseconds. If `immediate` is passed, trigger the function on the
+    // leading edge, instead of the trailing.
+    debounce: function (func, wait, immediate) {
+      var timeout;
+      return function() {
+        var context = this, args = arguments;
+        var later = function() {
+          timeout = null;
+          if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+      }
     }
   };
 }.call(this));
