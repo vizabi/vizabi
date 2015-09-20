570d86c69dc08f518a2a5070f47fd1482681fd37
Angie Skazka <konzeptmeister@gmail.com>
2015-09-20 22:26:27 +0200

    ammend last

--- src/base/utils.js
+++ src/base/utils.js
@@ -121,7 +121,7 @@
         var scrollable = ["scroll", "auto"];
         
         while(no && no.tagName !== "HTML" && scrollable.indexOf(no.style.overflow) == -1 ) {
-            console.log(no)
+            console.log(no, no.style.overflow)
             no = no.parentNode;
         }
 
