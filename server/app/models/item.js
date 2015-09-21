6eed83cd900a5ef8755eb80df589109ee203b914
Arthur Camara <arthurcamara@gmail.com>
2015-09-21 23:41:25 +0200

    Add related items

--- server/app/models/item.js
+++ server/app/models/item.js
@@ -1,9 +1,20 @@
-var mongoose = require('mongoose');
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+mongoose.model('RelatedItem', {
+  title : {type : String, default: ''},
+  subtitle : {type : String, default: ''},
+  image : {type : String, default: ''},
+  link : {type : String, default: ''},
+  _relatedTo: [{ type: Schema.Types.ObjectId, ref: 'Item' }]
+});
 
 module.exports = mongoose.model('Item', {
 	title : {type : String, default: ''},
 	category : {type : String, default: ''},
 	description : {type : String, default: ''},
+  image : {type : String, default: ''},
 	tool : {type : String, default: 'BubbleChart'},
-	opts : {type : Object, default: {}}
+	opts : {type : Object, default: {}},
+  relateditems : [{ type: Schema.Types.ObjectId, ref: 'RelatedItem' }]
 });
--- server/app/routes.js
+++ server/app/routes.js
@@ -1,54 +1,86 @@
 var Item = require('./models/item');
+var Menu = require('./models/menu');
+
+function getAll(model, res) {
+    model.find(function(err, items) {
+        if (err)
+            res.send(err);
+        res.json(items);
+    });
+}
+
+function getOne(model, conditions, res) {
+    model.findOne(conditions, function(err, item) {
+        if (err)
+            res.send(err);
+        res.json(item);
+    });
+}
+
 
 function getItems(res) {
-  Item.find(function(err, items) {
-    if (err)
-      res.send(err)
-    res.json(items); // return all todos in JSON format
-  });
+    Item.find()
+        .populate('relateditems')
+        .exec(function(err, items) {
+            if (err)
+                res.send(err);
+            res.json(items);
+        }));
+};
+
+
+function getMenu(which, res) {
+    getOne(Menu, {
+        "menu_label": which
+    }, res);
 };
 
 
 module.exports = function(app) {
 
-  /* API Routes */
+    /* API Routes */
 
-  //get all items in the database
-  app.get('/api/item', function(req, res) {
-      getItems(res);
-  });
-
-  //insert an item to the database
-  app.post('/api/item', function(req, res) {
-    Item.create({
-      title: req.body.title,
-      category: req.body.category,
-      description: req.body.description,
-      tool: req.body.tool,
-      opts: req.body.opts
-    }, function(err, item) {
-      if (err)
-        res.send(err);
-      // return all items
-      getItems(res);
+    //get all items in the database
+    app.get('/api/item', function(req, res) {
+        getItems(res);
     });
 
-  });
+    //insert an item to the database
+    app.post('/api/item', function(req, res) {
+        Item.create({
+            title: req.body.title,
+            category: req.body.category,
+            description: req.body.description,
+            tool: req.body.tool,
+            opts: req.body.opts
+        }, function(err, item) {
+            if (err)
+                res.send(err);
+            // return all items
+            getItems(res);
+        });
 
-  // delete an item
-  app.delete('/api/item/:item_id', function(req, res) {
-    Item.remove({
-      _id: req.params.item_id
-    }, function(err, item) {
-      if (err)
-        res.send(err);
-      // return all items
-      getItems(res);
     });
-  });
 
-  /* APP Routes */
-  app.get('*', function(req, res) {
-  	res.sendfile('./client/dist/index.html'); // load the single view file
-  });
+    // delete an item
+    app.delete('/api/item/:item_id', function(req, res) {
+        Item.remove({
+            _id: req.params.item_id
+        }, function(err, item) {
+            if (err)
+                res.send(err);
+            // return all items
+            getItems(res);
+        });
+    });
+
+    //get menu in the database
+    app.get('/api/menu', function(req, res) {
+        getMenu("Home", res);
+    });
+
+    /* APP Routes */
+    app.get('*', function(req, res) {
+        res.sendfile('./client/dist/index.html'); // load the single view file
+    });
 };
