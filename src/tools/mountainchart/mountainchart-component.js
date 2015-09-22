f5d01862f7d43900db56c54fc8ef472e127e7c34
Angie Skazka <konzeptmeister@gmail.com>
2015-09-22 16:34:51 +0200

    Change margins on mountain chart Reduce space between time-slider and x-axis, closes https://github.com/Gapminder/gapminder-tools-vizabi/issues/34

--- src/tools/mountainchart/mountainchart-component.js
+++ src/tools/mountainchart/mountainchart-component.js
@@ -763,16 +763,13 @@
 
             switch (this.getLayoutProfile()) {
             case 'small':
-                margin = { top: 10, right: 10, left: 10, bottom: 40 };
-//                margin = {top: 30, right: 20, left: 40, bottom: 40}
+                margin = { top: 10, right: 10, left: 10, bottom: 25 };
                 break;
             case 'medium':
-                margin = { top: 20, right: 10, left: 10, bottom: 70 };
-//                margin = {top: 30, right: 60, left: 60, bottom: 70}
+                margin = { top: 20, right: 20, left: 20, bottom: 30 };
                 break;
             case 'large':
-                margin = { top: 30, right: 10, left: 10, bottom: 90  };
-//                margin = {top: 30, right: 60, left: 60, bottom: 60}
+                margin = { top: 30, right: 30, left: 30, bottom: 35  };
                 break;
             }
 
