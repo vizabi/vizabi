1d71c7fb5c87e0b9f56948077f632727f195a05c
Dmitriy Shekhovtsov <valorkin@gmail.com>
2015-09-22 18:41:44 +0300

    chore(api): fixed if(err) issues, added .lean(true) to generic mongoose queries

--- server/app/routes.js
+++ server/app/routes.js
@@ -1,5 +1,7 @@
 var _ = require('lodash');
+var path = require('path');
 var cache = require('express-redis-cache')();
+var express = require('express');
 var mongoose = require('mongoose');
 var compression = require('compression');
 
@@ -13,9 +15,6 @@ var Indicators = mongoose.model('Indicators');
 
 var ObjectId = mongoose.Types.ObjectId;
 
-
-
-
 var BASEURL = '/tools/';
 
 function getAll(res, model, populate) {
@@ -24,7 +23,7 @@ function getAll(res, model, populate) {
     found = found.populate(populate);
   }
 
-  found.exec(function (err, items) {
+  return found.exec(function (err, items) {
     if (err) {
       return res.send(err);
     }
@@ -34,57 +33,58 @@ function getAll(res, model, populate) {
 }
 
 function getOne(res, model, conditions, populate) {
-  var found = model.findOne(conditions);
-  if (populate) found = found.populate(populate);
-  found.exec(function (err, item) {
-    if (err)
-      res.send(err);
-    res.json(item);
+  var found = model.findOne(conditions).lean(true);
+  if (populate) {
+    found = found.populate(populate);
+  }
+
+  return found.exec(function (err, item) {
+    if (err) {
+      return res.send(err);
+    }
+    return res.json(item);
   });
 }
 
 
 function getRelatedItems(res) {
-  getAll(res, RelatedItem, '_relatedTo');
-};
+  return getAll(res, RelatedItem, '_relatedTo');
+}
 
 function getRelatedItem(res, id) {
-  getOne(res, RelatedItem, {"_id": ObjectId(id)}, '_relatedTo');
-};
+  return getOne(res, RelatedItem, {"_id": ObjectId(id)}, '_relatedTo');
+}
 
 function getItems(res) {
-  getAll(res, Item, 'relateditems');
-};
+  return getAll(res, Item, 'relateditems');
+}
 
 function getItem(res, id) {
-  getOne(res, Item, {"_id": ObjectId(id)}, 'relateditems');
-};
-
+  return getOne(res, Item, {"_id": ObjectId(id)}, 'relateditems');
+}
 
 function getMenu(which, res) {
-  getOne(res, Menu, {
+  return getOne(res, Menu, {
     "menu_label": which
   });
-};
+}
 
 
 module.exports = function (app) {
-
-  console.log(BASEURL + 'api/item');
-
+  var router = express.Router();
   /* API Routes */
 
   //get all items in the database
-  app.get(BASEURL + 'api/item', function (req, res) {
-    getItems(res);
+  router.get('/item', function (req, res) {
+    return getItems(res);
   });
 
-  app.get(BASEURL + 'api/item/:item_id', function (req, res) {
-    getItem(res, req.params.item_id);
+  router.get('/item/:item_id', function (req, res) {
+    return getItem(res, req.params.item_id);
   });
 
   //insert an item to the database
-  app.post(BASEURL + 'api/item', function (req, res) {
+  router.post('/item', function (req, res) {
     Item.create({
       title: req.body.title,
       category: req.body.category,
@@ -92,42 +92,44 @@ module.exports = function (app) {
       tool: req.body.tool,
       opts: req.body.opts
     }, function (err, item) {
-      if (err)
-        res.send(err);
-      // return all items
-      getItems(res);
-    });
+      if (err){
+        return res.send(err);
+      }
 
+      // return all items
+      return getItems(res);
+    });
   });
 
   // delete an item
-  app.delete(BASEURL + 'api/item/:item_id', function (req, res) {
+  router.delete('/item/:item_id', function (req, res) {
     Item.remove({
       _id: req.params.item_id
     }, function (err, item) {
-      if (err)
-        res.send(err);
+      if (err) {
+        return res.send(err);
+      }
       // return all items
-      getItem(res, item._id);
+      return getItem(res, item._id);
     });
   });
 
   //get menu in the database
-  app.get(BASEURL + 'api/menu', function (req, res) {
-    getMenu("Home", res);
+  router.get('/menu', function (req, res) {
+    return getMenu("Home", res);
   });
 
   //get all related items in the database
-  app.get(BASEURL + 'api/related', function (req, res) {
-    getRelatedItems(res);
+  router.get('/related', function (req, res) {
+    return getRelatedItems(res);
   });
 
-  app.get(BASEURL + 'api/related/:related_id', function (req, res) {
-    getRelatedItem(res, req.params.related_id);
+  router.get('/related/:related_id', function (req, res) {
+    return getRelatedItem(res, req.params.related_id);
   });
 
   //insert a related item to the database
-  app.post(BASEURL + 'api/related', function (req, res) {
+  app.post('/related', function (req, res) {
 
     var relatedTo = req.body._relatedTo.split(",").map(function (id) {
       return ObjectId(id);
@@ -140,16 +142,14 @@ module.exports = function (app) {
       link: req.body.link,
       _relatedTo: relatedTo
     }, function (err, related_item) {
-      if (err)
-        res.send(err);
+      if (err) {
+        return res.send(err);
+      }
 
       var new_id = related_item._id;
-
-      var items = related_item._relatedTo,
-        s = items.length,
-        i;
-
-      for (i = 0; i < s; i++) {
+      var items = related_item._relatedTo;
+      var s = items.length;
+      for (var i = 0; i < s; i++) {
         Item.findOne({"_id": ObjectId(items[i])}, function (err, item) {
           item.relateditems = item.relateditems.map(function (id) {
             return ObjectId(id);
@@ -159,13 +159,12 @@ module.exports = function (app) {
         });
       }
 
-      getRelatedItem(res, new_id);
+      return getRelatedItem(res, new_id);
     });
-
   });
 
   // indicators data source
-  app.get(BASEURL + 'api/indicators/stub', compression(), cache.route({expire: 300}), function (req, res, next) {
+  router.get('/indicators/stub', compression(), cache.route({expire: 300}), function (req, res, next) {
     return Indicators.find().sort({time: 1}).lean().exec(function (err, indicatorValues) {
       if (err) {
         return res.send(err);
@@ -182,6 +181,9 @@ module.exports = function (app) {
       return res.json({success: true, data: data});
     });
   });
+
+  var base = path.join(BASEURL, 'api');
+  app.use(base, router);
 
   /* APP Routes */
   app.get('*', function (req, res) {
