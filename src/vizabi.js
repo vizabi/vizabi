25c87212b446b9263efcc1b05ee44c685a7d0a6a
Arthur Camara <arthurcamara@gmail.com>
2015-10-02 14:01:44 +0200

    Move custom build to outer folder

--- src/vizabi.js
+++ src/vizabi.js
@@ -1,68 +1,71 @@
-/*!
- * VIZABI MAIN
+import utils from './base/utils';
+import Tool from './base/tool';
+import Component from './base/component';
+import Model from './base/model';
+import Reader from './base/reader';
+import globals from './base/globals';
+import BubbleChart from './tools/bubblechart/bubblechart';
+
+var Vizabi = function(tool, placeholder, options) {
+  return startTool(tool, placeholder, options);
+};
+
+//TODO: make this configurable
+Vizabi._version = "0.8.1";
+
+//stores reference to each tool on the page
+Vizabi._instances = {};
+
+//stores global variables accessible by any tool or component
+Vizabi._globals = globals;
+
+//stores global variables accessible by any tool or component
+
+function startTool(name, placeholder, options) {
+  var tool = Tool.get(name);
+  if(tool) {
+    var t = new tool(placeholder, options);
+    Vizabi._instances[t._id] = t;
+    return t;
+  } else {
+    utils.error('Tool "' + name + '" was not found.');
+  }
+}
+
+//TODO: clear all objects and intervals as well
+//garbage collection
+Vizabi.clearInstances = function(id) {
+  if(id) {
+    Vizabi._instances[id] = void 0;
+  } else {
+    for(var i in Vizabi._instances) {
+      Vizabi._instances[i].clear();
+    }
+    Vizabi._instances = {};
+  }
+};
+
+console.log(BubbleChart);
+
+/*
+ * throws a warning if the required variable is not defined
+ * returns false if the required variable is not defined
+ * returns true if the required variable is defined
+ * @param variable
+ * @returns {Boolean}
  */
-(function () {
-  'use strict';
-  var root = this;
-  var previous = root.Vizabi;
-
-  var Vizabi = function (tool, placeholder, options) {
-    return startTool(tool, placeholder, options);
-  };
-  //stores reference to each tool on the page
-  Vizabi._instances = {};
-
-  //stores global variables accessible by any tool or component
-  Vizabi._globals = {};
-
-  function startTool(name, placeholder, options) {
-    var Tool = Vizabi.Tool.get(name);
-    if (Tool) {
-      var t = new Tool(placeholder, options);
-      Vizabi._instances[t._id] = t;
-      return t;
-    } else {
-      Vizabi.utils.error('Tool "' + name + '" was not found.');
-    }
+Vizabi._require = function(variable) {
+  if(typeof root[variable] === 'undefined') {
+    utils.warn(variable + ' is required and could not be found.');
+    return false;
   }
+  return true;
+};
 
-  //TODO: clear all objects and intervals as well
-  //garbage collection
-  Vizabi.clearInstances = function (id) {
-    if (id) {
-      Vizabi._instances[id] = void 0;
-    } else {
-      for(var i in Vizabi._instances) {
-        Vizabi._instances[i].clear();
-      }
-      Vizabi._instances = {};
-    }
-  };
+//makes all objects accessible
+Vizabi.Tool = Tool;
+Vizabi.Component = Component;
+Vizabi.Model = Reader;
+Vizabi.Reader = Reader;
 
-  /*
-   * throws a warning if the required variable is not defined
-   * returns false if the required variable is not defined
-   * returns true if the required variable is defined
-   * @param variable
-   * @returns {Boolean}
-   */
-  Vizabi._require = function (variable) {
-    if (typeof root[variable] === 'undefined') {
-      Vizabi.utils.warn(variable + ' is required and could not be found.');
-      return false;
-    }
-    return true;
-  };
-
-  //if AMD define
-  if (typeof define === 'function' && define.amd) {
-    define(function () {
-      return Vizabi;
-    });
-  } else if (typeof module === 'object' && module.exports) {
-    module.exports = Vizabi;
-  }
-
-  root.Vizabi = Vizabi;
-
-}.call(this));
+export default Vizabi;
