e103244e9c5716ca1843be272af3e94542f56f46
fahui.jdk <fahui.jdk@taobao.com>
2015-10-14 12:52:39 +0800

    bubble map chart init

---
+++ src/tools/bubblemapchart/bubblemapchart-component.js
@@ -0,0 +1,267 @@
+import * as utils from 'base/utils';
+import Component from 'base/component';
+import globals from 'base/globals'; // to get map data path
+
+import topojson from 'helpers/topojson';
+import d3_geo_projection from 'helpers/d3.geo.projection';
+
+//BAR CHART COMPONENT
+var BubbleMapChartComponent = Component.extend({
+  /**
+   * Initializes the component (Bubble Map Chart).
+   * Executed once before any template is rendered.
+   * @param {Object} config The options passed to the component
+   * @param {Object} context The component's parent
+   */
+  init: function (config, context) {
+    this.name = 'bubblemapchart';
+    this.template = 'bubblemapchart.html';
+
+    //define expected models for this component
+    this.model_expects = [{
+      name: "time",
+      type: "time"
+    }, {
+      name: "entities",
+      type: "entities"
+    }, {
+      name: "marker",
+      type: "model"
+    }, {
+      name: "language",
+      type: "language"
+    }];
+
+    var _this = this;
+    this.model_binds = {
+      "change:time:value": function (evt) {
+        _this.updateEntities();
+      }
+    };
+
+    //contructor is the same as any component
+    this._super(config, context);
+
+    this.xScale = null;
+    this.yScale = null;
+    this.cScale = d3.scale.category10();
+
+    d3_geo_projection();
+  },
+
+
+  afterPreload: function(){
+      var _this = this;
+      // TODO: add url to config or local
+      d3.json(globals.gapminder_paths.baseUrl + "data/world-50m.json", function(error, world) {
+        if (error) throw error;
+        _this.world = world;
+      });
+  },
+
+  /**
+   * DOM is ready
+   */
+  readyOnce: function () {
+
+    this.element = d3.select(this.element);
+
+    this.graph = this.element.select('.vzb-bc-graph');
+    this.bars = this.graph.select('.vzb-bc-bars');
+
+    var _this = this;
+    this.on("resize", function () {
+      _this.updateEntities();
+    });
+  },
+
+  /*
+   * Both model and DOM are ready
+   */
+  ready: function () {
+    this.updateIndicators();
+    this.resize();
+    this.updateEntities();
+  },
+
+  /**
+   * Changes labels for indicators
+   */
+  updateIndicators: function () {
+    var _this = this;
+    this.translator = this.model.language.getTFunction();
+    this.duration = this.model.time.speed;
+
+    this.yScale = this.model.marker.axis_y.getScale();
+    this.xScale = this.model.marker.axis_x.getScale();
+    this.cScale = this.model.marker.color.getScale();
+  },
+
+  /**
+   * Updates entities
+   */
+  updateEntities: function () {
+
+    var _this = this;
+    var time = this.model.time;
+    var timeDim = time.getDimension();
+    var entityDim = this.model.entities.getDimension();
+    var duration = (time.playing) ? time.speed : 0;
+    var filter = {};
+    filter[timeDim] = time.value;
+    var items = this.model.marker.getKeys(filter);
+    var values = this.model.marker.getValues(filter, [entityDim]);
+
+    // TODO: add to csv
+    //Africa 9.1021° N, 18.2812°E 
+    //Europe 53.0000° N, 9.0000° E
+    //Asia 49.8380° N, 105.8203° E
+    //north American 48.1667° N and longitude 100.1667° W
+    var pos = {
+      "afr": {lat: 9.1, lng: 18.3},
+      "eur": {lat: 53.0, lng: 9.0},
+      "asi": {lat: 49.8, lng: 105.8},
+      "ame": {lat: 48.2, lng: -100.2},
+    };
+
+    this.entityBars = this.bars.selectAll('.vzb-bc-bar')
+      .data(items);
+
+    //exit selection
+    this.entityBars.exit().remove();
+
+    //enter selection -- init circles
+    this.entityBars.enter().append("circle")
+      .attr("class", "vzb-bc-bar")
+      .on("mousemove", function (d, i) {
+      })
+      .on("mouseout", function (d, i) {
+      })
+      .on("click", function (d, i) {
+      });
+
+    //positioning and sizes of the bars
+
+    var bars = this.bars.selectAll('.vzb-bc-bar');
+    var barWidth = this.xScale.rangeBand();
+
+    this.bars.selectAll('.vzb-bc-bar')
+      .attr("fill", function (d) {
+        return _this.cScale(values.color[d[entityDim]]);
+      })
+      .attr("cx", function (d) {
+        //return _this.xScale(values.axis_x[d[entityDim]]);
+        return _this.projection([pos[d[entityDim]].lng, pos[d[entityDim]].lat])[0];
+      })
+      .transition().duration(duration).ease("linear")
+      .attr("cy", function (d) {
+        //return _this.yScale(values.axis_y[d[entityDim]]);
+        return _this.projection([pos[d[entityDim]].lng, pos[d[entityDim]].lat])[1];
+      })
+      .attr("r", function (d) {
+        return _this.yScale(values.axis_y[d[entityDim]]);
+      });
+  },
+
+  /**
+   * Executes everytime the container or vizabi is resized
+   * Ideally,it contains only operations related to size
+   */
+  resize: function () {
+
+    var _this = this;
+
+    this.profiles = {
+      "small": {
+        margin: {
+          top: 30,
+          right: 20,
+          left: 40,
+          bottom: 50
+        },
+        padding: 2,
+        minRadius: 2,
+        maxRadius: 40
+      },
+      "medium": {
+        margin: {
+          top: 30,
+          right: 60,
+          left: 60,
+          bottom: 60
+        },
+        padding: 2,
+        minRadius: 3,
+        maxRadius: 60
+      },
+      "large": {
+        margin: {
+          top: 30,
+          right: 60,
+          left: 60,
+          bottom: 80
+        },
+        padding: 2,
+        minRadius: 4,
+        maxRadius: 80
+      }
+    };
+
+    this.activeProfile = this.profiles[this.getLayoutProfile()];
+    var margin = this.activeProfile.margin;
+
+
+    //stage
+    var height = this.height = parseInt(this.element.style("height"), 10) - margin.top - margin.bottom;
+    var width = this.width = parseInt(this.element.style("width"), 10) - margin.left - margin.right;
+
+    this.graph
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    //update scales to the new range
+    // TODO: r ration should add to config
+    if (this.model.marker.axis_y.scaleType !== "ordinal") {
+      this.yScale.range([0, this.height / 4]);
+    } else {
+      this.yScale.rangePoints([0, this.height / 4], _this.activeProfile.padding).range();
+    }
+
+
+
+    // map background
+    var world = this.world;
+    var projection = this.projection = d3.geo.patterson()
+        .scale(153 / 960 * width)
+        .translate([width / 2, height / 2])
+        .precision(.1);
+
+    var path = this.bgPath = d3.geo.path()
+        .projection(projection);
+
+    var graticule = d3.geo.graticule();
+
+    var svg = this.bgSvg = d3.select(".vzb-bc-bars-background")
+        .attr("width", width)
+        .attr("height", height);
+    svg.html('');
+
+    svg.append("path")
+        .datum(graticule)
+        .attr("class", "graticule")
+        .attr("d", path);
+
+    svg.insert("path", ".graticule")
+        .datum(topojson.feature(world, world.objects.land))
+        .attr("class", "land")
+        .attr("d", path);
+
+    svg.insert("path", ".graticule")
+        .datum(topojson.mesh(world, world.objects.countries, function(a, b) { return a !== b; }))
+        .attr("class", "boundary")
+        .attr("d", path);
+
+  }
+});
+
+
+export default BubbleMapChartComponent;
---
+++ src/tools/bubblemapchart/bubblemapchart.js
@@ -0,0 +1,95 @@
+/*!
+ * VIZABI BUBBLEMAPCHART
+ */
+
+import * as utils from 'base/utils';
+import Tool from 'base/tool';
+
+import BubbleMapChartComponent from './bubblemapchart-component';
+
+import {
+  timeslider,
+  buttonlist,
+  treemenu
+}
+from 'components/_index';
+
+//BAR CHART TOOL
+var BubbleMapChart = Tool.extend('BubbleMapChart', {
+
+
+  /**
+   * Initializes the tool (Bar Chart Tool).
+   * Executed once before any template is rendered.
+   * @param {Object} config Initial config, with name and placeholder
+   * @param {Object} options Options such as state, data, etc
+   */
+  init: function(config, options) {
+
+    this.name = "bubblemapchart";
+
+    //specifying components
+    this.components = [{
+      component: BubbleMapChartComponent,
+      placeholder: '.vzb-tool-viz',
+      model: ["state.time", "state.entities", "state.marker", "language"] //pass models to component
+    }, {
+      component: timeslider,
+      placeholder: '.vzb-tool-timeslider',
+      model: ["state.time"]
+    }
+    /*, {
+      component: buttonlist,
+      placeholder: '.vzb-tool-buttonlist',
+      model: ['state', 'ui', 'language']
+    }, {
+      component: treemenu,
+      placeholder: '.vzb-tool-treemenu',
+      model: ['state.marker', 'language']
+    }*/
+    ];
+
+    //constructor is the same as any tool
+    this._super(config, options);
+  },
+
+  default_options: {
+    state: {
+      time: {},
+      entities: {
+        dim: "geo",
+        show: {
+          _defs_: {
+            "geo": ["*"],
+            "geo.cat": ["region"]
+          }
+        }
+      },
+      marker: {
+        space: ["entities", "time"],
+        label: {
+          use: "property",
+          which: "geo.name"
+        },
+        axis_y: {
+          use: "indicator",
+          which: "lex"
+        },
+        axis_x: {
+          use: "property",
+          which: "geo.name"
+        },
+        color: {
+          use: "property",
+          which: "geo.region"
+        }
+      }
+    },
+    data: {
+      reader: "csv",
+      path: "data/waffles/basic-indicators.csv"
+    }
+  }
+});
+
+export default BubbleMapChart;
