237db9a1eb9f7bad0f45abba3450cc342d19d826
fahui.jdk <fahui.jdk@taobao.com>
2015-11-16 02:16:19 +0800

    bubble map add buttons

--- src/vizabi-gapminder.js
+++ src/vizabi-gapminder.js
@@ -160,6 +160,13 @@ BarRankChart.define('default_options', {
   }
 });
 
+BubbleMapChart.define('datawarning_content', {
+  title: "",
+  body: "Comparing the size of economy across countries and time is not trivial. The methods vary and the prices change. Gapminder has adjusted the picture for many such differences, but still we recommend you take these numbers with a large grain of salt.<br/><br/> Countries on a lower income levels have lower data quality in general, as less resources are available for compiling statistics. Historic estimates of GDP before 1950 are generally also more rough. <br/><br/> Data for child mortality is more reliable than GDP per capita, as the unit of comparison, dead children, is universally comparable across time and place. This is one of the reasons this indicator has become so useful to measure social progress. But the historic estimates of child mortality are still suffering from large uncertainties.<br/><br/> Learn more about the datasets and methods in this <a href='http://www.gapminder.org/news/data-sources-dont-panic-end-poverty' target='_blank'>blog post</a>",
+  doubtDomain: [1800, 1950, 2015],
+  doubtRange: [1.0, .3, .2]
+});
+
 BubbleMapChart.define('default_options', {
   state: {
     time: {
@@ -189,13 +196,11 @@ BubbleMapChart.define('default_options', {
         use: "indicator",
         which: "pop",
         scaleType: "linear",
-        /*
-        min: 1,
-        max: 90,
-        */
         allow: {
           scales: ["linear", "log"]
-        }
+        },
+        min: .04,
+        max: .90
       },
       lat: {
         use: "property",
