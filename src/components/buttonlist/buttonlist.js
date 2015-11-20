107901eafe2761e3456e83ae0d7418497763c441
“Hugo <hucharal13@gmail.com>
2015-11-20 14:40:29 -0200

    add print button for bubblechart

--- src/components/buttonlist/buttonlist.js
+++ src/components/buttonlist/buttonlist.js
@@ -133,6 +133,13 @@ var ButtonList = Component.extend({
         ispin: false,
         required: false
       },
+      'print': {
+        title: "buttons/print",
+        icon: "print",
+        dialog: false,
+        func: this.togglePrintVersion.bind(this),
+        required: true
+      },
       '_default': {
         title: "Button",
         icon: "asterisk",
@@ -704,6 +711,9 @@ var ButtonList = Component.extend({
       event.eventName = "resize";
       window.dispatchEvent(event);
     })();
+  },
+  togglePrintVersion: function(id) {
+    return true;
   }
 
 });
