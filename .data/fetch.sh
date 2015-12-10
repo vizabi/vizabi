2afd1101762881b3b85a96f99c4a98a13f41337d
Angie Skazka <konzeptmeister@gmail.com>
2015-12-10 14:29:31 +0100

    Change gdp_per_cap to gdp_pc, edit metadata indicators and indicatorTree, edit corresponding language strings

--- .data/fetch.sh
+++ .data/fetch.sh
@@ -11,7 +11,7 @@ languages=( en fa pt )
 for i in "${languages[@]}"
 do
 echo "Fetching data for language: "$i
-curl -s -H "Content-Type: application/json" -d '{"query":[{"SELECT":["geo","geo.name","time","geo.region", "geo.cat","lex","gdp_per_cap","pop"],"FROM":"humnum","WHERE":{"geo":["*"],"geo.cat":["*"],"time":["1990-2014"]}}],"language":"'$i'"}'  http://waffle.gapminder.org/api/v1/query > waffles/$i/basic-indicators.json
+curl -s -H "Content-Type: application/json" -d '{"query":[{"SELECT":["geo","geo.name","time","geo.region", "geo.cat","lex","gdp_pc","pop"],"FROM":"humnum","WHERE":{"geo":["*"],"geo.cat":["*"],"time":["1990-2014"]}}],"language":"'$i'"}'  http://waffle.gapminder.org/api/v1/query > waffles/$i/basic-indicators.json
 
 curl -s -H "Content-Type: application/json" -d '{"query":[{"SELECT":["geo","geo.name", "geo.region", "geo.cat"],"FROM":"humnum","WHERE":{"geo":["*"],"geo.cat":["*"]}}],"language":"'$i'"}'  http://waffle.gapminder.org/api/v1/query > waffles/$i/names.json
 echo "SAVED!"
