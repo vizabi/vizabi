a80806370be17aa9fd0a6a58f9e29114dd3c0868
Dmitry Basov <dmbasov@gmail.com>
2015-12-19 13:36:21 +0700

    Auto-generated "more options" dialog. #797

--- src/components/dialogs/moreoptions/moreoptions.js
+++ src/components/dialogs/moreoptions/moreoptions.js
@@ -16,7 +16,7 @@ from 'components/_index';
  * More options dialog
  */
 
-export default Dialog.extend({
+var MoreOptions = Dialog.extend({
 
   /**
    * Initializes the dialog component
@@ -25,104 +25,35 @@ export default Dialog.extend({
    */
   init: function(config, parent) {
     this.name = 'moreoptions';
-
-    this.components = [{
-      component: indicatorpicker,
-      placeholder: '.vzb-xaxis-selector',
-      model: ["state.marker", "language"],
-      markerID: "axis_x"
-    },{
-      component: minmaxinputs,
-      placeholder: '.vzb-xaxis-minmax',
-      model: ["state.marker", "language"],
-      markerID: "axis_x",
-      ui: {
-        selectMinMax: false,
-        selectFakeMinMax: true
-      }
-    }, {
-      component: indicatorpicker,
-      placeholder: '.vzb-yaxis-selector',
-      model: ["state.marker", "language"],
-      markerID: "axis_y"
-    }, {
-      component: minmaxinputs,
-      placeholder: '.vzb-yaxis-minmax',
-      model: ["state.marker", "language"],
-      markerID: "axis_y",
-      ui: {
-        selectMinMax: false,
-        selectFakeMinMax: true
-      }
-    }, {
-      component: simplecheckbox,
-      placeholder: '.vzb-axes-options',
-      model: ["state", "language"],
-      submodel: 'time',
-      checkbox: 'adaptMinMaxZoom'
-    }, {
-      component: indicatorpicker,
-      placeholder: '.vzb-saxis-selector',
-      model: ["state.marker", "language"],
-      markerID: "size"
-    }, {
-      component: bubblesize,
-      placeholder: '.vzb-dialog-bubblesize',
-      model: ["state.marker.size"],
-      ui: {
-        show_button: false
-      }
-    }, {
-      component: indicatorpicker,
-      placeholder: '.vzb-caxis-selector',
-      model: ["state.marker", "language"],
-      markerID: "color"
-    }, {
-      component: colorlegend,
-      placeholder: '.vzb-clegend-container',
-      model: ["state.marker.color", "state.entities", "language"]
-    }, {
-      component: simpleslider,
-      placeholder: '.vzb-dialog-bubbleopacity-regular',
-      model: ["state.entities"],
-      arg: "opacityRegular"
-    }, {
-      component: simpleslider,
-      placeholder: '.vzb-dialog-bubbleopacity-selectdim',
-      model: ["state.entities"],
-      arg: "opacitySelectDim"
-    }, {
-      component: simpleslider,
-      placeholder: '.vzb-dialog-delay-slider',
-      model: ["state.time"],
-      arg: "delay",
-      properties: {min:1, max:5, step:0.1, scale: d3.scale.linear()
-        .domain([1,2,3,4,5])
-        .range([1200,900,450,200,75])
-      }
-    },
-    {
-      component: simplecheckbox,
-      placeholder: '.vzb-presentationmode-switch',
-      model: ["ui", "language"],
-      checkbox: 'presentation'
-    }];
-
+    
     this._super(config, parent);
   },
 
   readyOnce: function() {
     var _this = this;
     this.element = d3.select(this.element);
+    this.contentEl = this.element.select('.vzb-dialog-content');
+    
+    var dialog_popup = this.model.ui.dialogs.popup || [];
+    var dialog_moreoptions = this.model.ui.dialogs['moreoptions'] || [];
+    
+    // if dialog_moreoptions has been passed in with boolean param or array must check and covert to array
+    if (dialog_moreoptions === true) {
+      dialog_moreoptions = dialog_popup;
+      this.model.ui.dialogs['moreoptions'] = dialog_moreoptions;
+    }
+    
+    this._addDialogs(dialog_moreoptions);
+
     this.resize();
     
     //accordion
     this.accordionEl = this.element.select('.vzb-accordion');
     if(this.accordionEl) {
-      this.accordionEl.selectAll('.vzb-accordion-section')
-        .select('.vzb-accordion-section-title')
-        .on('click', function() {
-          var sectionEl = d3.select(this.parentNode);
+      var titleEl = this.accordionEl.selectAll('.vzb-accordion-section')
+        .select('.vzb-dialog-title>span:first-child')
+      titleEl.on('click', function(d) {
+          var sectionEl = _this.components[d.component].placeholderEl;
           var activeEl = _this.accordionEl.select('.vzb-accordion-active');
           if(activeEl) {
             activeEl.classed('vzb-accordion-active', false);
@@ -136,9 +67,63 @@ export default Dialog.extend({
 
   resize: function() {
     var totalHeight = this.root.element.offsetHeight - 200;
-    var content = this.element.select('.vzb-dialog-content');
-    content.style('max-height', totalHeight + 'px');
+    this.contentEl.style('max-height', totalHeight + 'px');
 
     this._super();
+  },
+  
+  _addDialogs: function(dialog_list) {
+    this._components_config = [];
+    var details_dlgs = [];
+    if(!dialog_list.length) return;
+    //add a component for each dialog
+    for(var i = 0; i < dialog_list.length; i++) {
+
+      //check moreoptions in dialog.moreoptions
+      if(dialog_list[i] === "moreoptions") continue;
+      
+      var dlg = dialog_list[i];
+      var dlg_config = utils.deepClone(this.parent._available_dialogs[dlg]);
+
+      //if it's a dialog, add component
+      if(dlg_config && dlg_config.dialog) {
+        var comps = this._components_config;
+
+        //add corresponding component
+        comps.push({
+          component: dlg_config.dialog,
+          placeholder: '.vzb-dialogs-dialog[data-dlg="' + dlg + '"]',
+          model: ["state", "ui", "language"]
+        });
+
+        dlg_config.component = comps.length - 1;
+      
+        dlg_config.id = dlg;
+        details_dlgs.push(dlg_config);
+      }
+    };
+
+    this.contentEl.selectAll('div').data(details_dlgs)
+      .enter().append("div")
+      .attr('class', function (d) {
+        var cls = 'vzb-dialogs-dialog vzb-moreoptions vzb-accordion-section';
+        return cls;
+      })
+      .attr('data-dlg', function(d) {
+        return d.id;
+      });
+
+    this.loadComponents();
+
+    var _this = this;
+    //render each subcomponent
+    utils.forEach(this.components, function(subcomp) {
+      subcomp.render();
+      _this.on('resize', function() {
+        subcomp.trigger('resize');
+      });
+    });
   }
 });
+
+export default MoreOptions;
