f563ae501cd5f4d78db4ffbb13e1ecf65b096b63
Jasper <github@jasperheeffer.nl>
2016-01-13 17:59:54 +0100

    Waffle reader fixes to work with timeranges and remove old query parsing code

--- src/base/utils.js
+++ src/base/utils.js
@@ -332,11 +332,7 @@ function isSpecificValue(val) {
 }
 
 function cloneSpecificValue(val) {
-  if (val instanceof Buffer) {
-    var x = new Buffer(val.length);
-    val.copy(x);
-    return x;
-  } else if (val instanceof Date) {
+  if (val instanceof Date) {
     return new Date(val.getTime());
   } else if (val instanceof RegExp) {
     return new RegExp(val);
