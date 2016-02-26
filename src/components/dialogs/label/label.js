d8dc2d8e48f3a15d15623625ab2b9bbada0a57ea
Dmitry Basov <dmbasov@gmail.com>
2016-02-26 21:32:05 +0700

    Label dialog

---
+++ src/components/dialogs/label/label.js
@@ -0,0 +1,51 @@
+import * as utils from 'base/utils';
+import Component from 'base/component';
+import Dialog from '../_dialog';
+
+import { sizeslider, indicatorpicker, simplecheckbox } from 'components/_index'
+/*
+ * Size dialog
+ */
+
+var Label = Dialog.extend({
+
+/**
+ * Initializes the dialog component
+ * @param config component configuration
+ * @param context component context (parent)
+ */
+init: function(config, parent) {
+  this.name = 'label';
+
+  // in dialog, this.model_expects = ["state", "data"];
+
+  this.components = [
+  {
+    component: sizeslider,
+    placeholder: '.vzb-dialog-bubblesize',
+    model: ["state.marker.size_label"],
+    propertyname: 'LabelTextSize',
+    ui: {
+      show_button: false
+    }
+  },
+  {
+    component: indicatorpicker,
+    placeholder: '.vzb-saxis-selector',
+    model: ["state.marker", "language"],
+    markerID: "size_label"
+  },
+  {
+    component: simplecheckbox,
+    placeholder: '.vzb-removelabelbox-switch',
+    model: ["ui.vzb-tool-bubblechart", "language"],
+    checkbox: 'removeLabelBox',
+    submodel: 'labels'
+  }
+  ];
+
+  this._super(config, parent);
+}
+});
+
+export default Label;
