6887f91ababea3424c28d5bcf74625e7b9279b24
Angie Skazka <konzeptmeister@gmail.com>
2016-03-09 10:34:21 +0100

    Move around the zoom instructions so adapting zoom to fit bubbles works on refresh

--- src/tools/bubblechart/bubblechart-component.js
+++ src/tools/bubblechart/bubblechart-component.js
@@ -470,12 +470,11 @@ var BubbleChartComp = Component.extend({
       _this.redrawDataPoints();
       _this.updateBubbleOpacity();
       _this.cached = {};
-      if(_this.model.time.adaptMinMaxZoom) _this._panZoom.expandCanvas();
-
       _this._trails.create();
       _this._trails.run("findVisible");
       _this.zoomToMarkerMaxMin(); // includes redraw data points and trail resize
       _this._trails.run(["recolor", "opacityHandler", "reveal"]);
+      if(_this.model.time.adaptMinMaxZoom) _this._panZoom.expandCanvas();
     });
   },
     
