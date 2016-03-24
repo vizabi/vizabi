28a67ab4207edc89165d0cafe36313f2bb7d097d
Angie Skazka <konzeptmeister@gmail.com>
2016-03-24 22:36:46 +0100

    Fix #1560: Make change of the log scale non-persistent when it's together with the change of "which"

--- src/components/treemenu/treemenu.js
+++ src/components/treemenu/treemenu.js
@@ -987,8 +987,12 @@ var TreeMenu = Component.extend({
 
     if(what == "which") {
       obj.use = indicatorsDB[value].use;
-      if(indicatorsDB[value].scales) obj.scaleType = indicatorsDB[value].scales[0];
-        
+              
+      if(indicatorsDB[value].scales) {
+          // change scale without triggering events (non-persistent)
+          // otherwise ready is called via bubblechart:72 change:marker:scaleType before data arrives
+          mdl.getModelObject('scaleType').set(indicatorsDB[value].scales[0], false, false); 
+      }
     }
 
     if(mdl.getType() === 'axis' || mdl.getType() === 'size') {
