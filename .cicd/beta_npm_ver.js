03513250df89fa086fb1f22f623f2d50a390148b
travis <travis@example.com>
2016-04-05 12:11:37 +0300

    Fix beta npm package CD versioning
      work around discrepancy of 'develop' branch
      and npm versions

--- .cicd/beta_npm_ver.js
+++ .cicd/beta_npm_ver.js
@@ -5,10 +5,14 @@
 const shell = require('shelljs');
 const cv = require('compare-version');
 const fs = require('fs');
+const path = require('path');
+
+const cwd = path.dirname(require.main.filename);
+const project_dir = path.dirname(cwd);
 
 const FLAGFILE = "/tmp/deployment.flag";
 
-const pjson = require('../package.json');
+const pjson = require(path.join(project_dir, 'package.json'));
 const oldPjson = JSON.parse(shell.exec("git show HEAD~1:package.json").output);
 
 const lastPublishedVersion = shell.exec("npm show "+pjson.name+" version").output.trim();
@@ -24,18 +28,19 @@ console.log("--- Latest published version: ", lastPublishedVersion);
 console.log("--- Previous package.json version: ", oldPjson.version);
 console.log("--- Current package.json version: ", pjson.version);
 
-let versionBump = (cv(oldPjson.version, pjson.version) !== 0);
+let versionBump = (cv(oldPjson.version, pjson.version) < 0);
 console.log("===== Bumped version:", versionBump);
 
 if (!versionBump){
     console.log("--- Publishing...");
-    if(cv(pjson.version, lastPublishedVersion) !== 0){
-        shell.exec("npm version " + lastPublishedVersion);
-    }
-    shell.exec("npm version --no-git-tag-version prerelease");
-    shell.exec("git status");
+    pjson.version = lastPublishedVersion;
+    fs.writeFileSync(path.join(project_dir, 'package.json'), JSON.stringify(pjson, null, 2));
     shell.exec("git add package.json");
     shell.exec("git config --global user.name travis");
     shell.exec("git config --global user.email travis@example.com");
+    shell.exec("git commit --amend -C $(git rev-parse --verify HEAD)");
+    shell.exec("npm version --no-git-tag-version prerelease");
+    shell.exec("git status");
+    shell.exec("git add package.json");
     shell.exec("git commit --amend -C $(git rev-parse --verify HEAD)");
 };
