6a5bca4a415bcf917be9e25c6f0c382faf11931c
Dmitry Basov <dmbasov@gmail.com>
2016-05-10 18:19:40 +0700

    Treemenu: Don't unfold the menu items in mobile mode when scrolling #1719

--- src/helpers/d3.touchEvents.js
+++ src/helpers/d3.touchEvents.js
@@ -1,14 +1,22 @@
 function detectTouchEvent(element, onTap, onLongTap) {
   var start;
+  var coordX;
+  var coordY;
   var namespace = onTap ? '.onTap' : '.onLongTap';
   d3.select(element)
     .on('touchstart' + namespace, function(d, i) {
       start = d3.event.timeStamp;
+      coordX = d3.event.changedTouches[0].screenX;
+      coordY = d3.event.changedTouches[0].screenY;
     })
     .on('touchend' + namespace, function(d, i) {
-      if(d3.event.timeStamp - start < 500)
-        return onTap ? onTap(d, i) : undefined;
-      return onLongTap ? onLongTap(d, i) : undefined;
+      coordX = Math.abs(coordX - d3.event.changedTouches[0].screenX);
+      coordY = Math.abs(coordY - d3.event.changedTouches[0].screenY);
+      if(coordX < 5 && coordY < 5) {
+        if(d3.event.timeStamp - start < 500)
+          return onTap ? onTap(d, i) : undefined;
+        return onLongTap ? onLongTap(d, i) : undefined;
+      } else return undefined;
     });
 }
 
