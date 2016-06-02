1dada2350093431aacd96c6e41626e3363d73a52
Zeeshan Saleemi <zeeshan.saleemi@kvf.local>
2016-06-02 15:59:46 +0500

    Testing

--- .cicd/protractor.conf.js
+++ .cicd/protractor.conf.js
@@ -3,7 +3,7 @@
 let config = {
     baseUrl: 'http://localhost:9000/',
   specs: [
-//    '../tests/e2e/*.spec.js'
+//   '../tests/e2e/*.spec.js'
       '../tests/e2e/excel_access.spec.js'
   ],
   exclude: [],
