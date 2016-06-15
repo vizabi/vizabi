d95e09831b3eb9e54fb0f036ca995271964dd603
abid.ali <abid.ali@kualitatem.com>
2016-06-15 16:33:20 +0500

    pageObjects.json file is fully updated for each spec

--- tests/e2e/linechart.spec.js
+++ tests/e2e/linechart.spec.js
@@ -1,16 +1,18 @@
 describe('Web - Vizabi e2e test :: Line Chart', function() {
 
+  var testData = require('../../pageObjects.json');    
   jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000000;
   browser.manage().window().setSize(1100, 600);
 
   var baseUrl = 'http://localhost:9000/preview/';
   var EC = protractor.ExpectedConditions;
+  testData.forEach( function (data) {
 
   // Base Selectors
 
-  var buttonPlay = element(by.css("#vzbp-placeholder > div > div.vzb-tool-stage > div.vzb-tool-timeslider > div > div.vzb-ts-btns > button.vzb-ts-btn-play.vzb-ts-btn > svg"));
-  var countries = element(by.css("#vzbp-placeholder > div > div.vzb-tool-stage > div.vzb-tool-viz > div > svg > g > svg > svg.vzb-lc-lines"));
-  var buttonList = element(by.css("#vzbp-placeholder > div > div.vzb-tool-sidebar > div.vzb-tool-buttonlist"));
+  var buttonPlay = element(by.css(data.All_Global_Loctors.buttonPlay_Locator_CSS));
+  var countries = element(by.css(data.line_Chart_Loctors.countriesLineChart_Locator_CSS));
+  var buttonList = element(by.css(data.All_Global_Loctors.buttonList_Locator_CSS));
 
   it('Loading Line Chart Page', function() {
 
@@ -52,4 +54,5 @@ describe('Web - Vizabi e2e test :: Line Chart', function() {
 
   });
 
+});
 });
