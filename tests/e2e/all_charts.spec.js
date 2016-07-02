b09203fa6af4f828f40962d10db20192d7e8a52d
Zeeshan Saleemi <zeeshan.saleemi@kvf.local>
2016-07-02 12:25:03 +0500

    Testing 'URLPersistencyMountainChart'

--- tests/e2e/all_charts.spec.js
+++ tests/e2e/all_charts.spec.js
@@ -263,6 +263,7 @@ describe('Web - Vizabi e2e test :: All', function() {
     browser.refresh();
     browser.wait(EC.visibilityOf(play), 120000 , "Chart is not Refreshed");
     browser.sleep(15000);
+
       
     //Getting Url After Refreshing the page  
     var urlAfter = browser.getCurrentUrl();
