769989f1c45f7402def2f7b54102a385ccf731a6
Angie Skazka <konzeptmeister@gmail.com>
2016-10-18 11:56:08 +0200

    Move and fix configs of the bar chart

--- src/vizabi-gapminder.js
+++ src/vizabi-gapminder.js
@@ -44,90 +44,12 @@ globals.ext_resources = utils.deepExtend({
 
 //OVERWRITE OPTIONS
 
-BarChart.define('default_model', {
-  state: {
-    time: {
-      start: "1800",
-      end: "2012",
-      value: "2000",
-      step: 1
-    },
-    entities: {
-      dim: "geo",
-      show: {
-        _defs_: {
-          "geo": ["usa", "swe", "nor"]
-        }
-      }
-    },
-    entities_minimap: {
-      dim: "geo",
-      show: {
-        _defs_: {
-          "is--world_4region": true
-        }
-      }
-    },
-    marker: {
-      space: ["entities", "time"],
-      label: {
-        use: "property",
-        which: "name"
-      },
-      axis_y: {
-        use: "indicator",
-        which: "sg_population", // systema globalis
-        //which: "population_total",
-        scaleType: "log",
-        allow: {
-          scales: ["linear", "log"]
-        }
-      },
-      axis_x: {
-        use: "property",
-        which: "name",
-        allow: {
-          scales: ["ordinal"],
-          names: ["!geo", "!_default"]
-        }
-      },
-      color: {
-        use: "property",
-        which: "world_4region",
-        scaleType: "ordinal"
-      }
-    },
-    marker_minimap:{
-      space: ["entities_minimap"],
-        type: "geometry",
-        shape: "svg",
-        label: {
-          use: "property",
-          which: "name"
-        },
-        geoshape: {
-          use: "property",
-          which: "shape_lores_svg"
-        }
-    }
-  },
-  data: {
-    reader: "csv",
-    path: globals.ext_resources.host + globals.ext_resources.dataPath + "dont-panic-poverty.csv"
-  },
-  language: language,
-  ui: {
-    presentation: false
-  }
-});
-
 BubbleMap.define('datawarning_content', {
   title: "",
   body: "Comparing the size of economy across countries and time is not trivial. The methods vary and the prices change. Gapminder has adjusted the picture for many such differences, but still we recommend you take these numbers with a large grain of salt.<br/><br/> Countries on a lower income levels have lower data quality in general, as less resources are available for compiling statistics. Historic estimates of GDP before 1950 are generally also more rough. <br/><br/> Data for child mortality is more reliable than GDP per capita, as the unit of comparison, dead children, is universally comparable across time and place. This is one of the reasons this indicator has become so useful to measure social progress. But the historic estimates of child mortality are still suffering from large uncertainties.<br/><br/> Learn more about the datasets and methods in this <a href='http://www.gapminder.org/news/data-sources-dont-panic-end-poverty' target='_blank'>blog post</a>",
   doubtDomain: [1800, 1950, 2015],
   doubtRange: [1.0, .3, .2]
 });
-
 
 MountainChart.define('datawarning_content', {
   title: "Income data has large uncertainty!",
