e3328afb87f049ca67b02db251c341844f9b3ba0
kuguarpwnz <kgrpwnz@gmail.com>
2016-11-07 12:03:46 +0200

    Fix donut chart

--- preview/assets/js/toolconfigs/DonutChart.js
+++ preview/assets/js/toolconfigs/DonutChart.js
@@ -1,34 +1,35 @@
-var VIZABI_MODEL = { 
+var VIZABI_MODEL = {
   state: {
     // available time would have the range of 1990-2012 years (%Y), with the deafult position at 2000
     time: {
-      start: "1990",
-      end: "2012",
-      value: "2000"
+      startOrigin: '1990',
+      endOrigin: '2012',
+      value: '2000'
     },
-    //Entities include all ("*") geo's of category "regions" -- equivalent to 'geo: ["asi", "ame", "eur", "afr"]'
+    // Entities include all ('*') geo's of category 'regions' -- equivalent to "geo: ['asi', 'ame', 'eur', 'afr']"
     entities: {
-      dim: "geo",
+      dim: 'geo',
       show: {
-          "geo": ["usa", "bra", "chn", "ind", "idn"]
+        geo: {
+          $in: ['usa', 'bra', 'chn', 'ind', 'idn']
+        }
       }
     },
-    //Markers correspond to visuals that we want to show. We have label, axis and color
+    // Markers correspond to visuals that we want to show. We have label, axis and color
     marker: {
-      space: ["entities", "time"],
+      space: ['entities', 'time'],
       label: {
-        use: "property",
-        which: "name"
+        use: 'property',
+        which: 'name'
       },
       axis: {
-        use: "indicator",
-        which: "sg_population"
+        use: 'indicator',
+        which: 'sg_population'
       },
       color: {
-        use: "property",
-        which: "world_4region"
+        use: 'property',
+        which: 'world_4region'
       }
     }
   }
-
-}
+};
