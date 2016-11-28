7ce0bfcae687df36e33614dd2277a6d438b1113d
Angie Skazka <konzeptmeister@gmail.com>
2016-11-28 18:17:44 +0100

    Group values in validation to set them together. This should lead to less validation cycles and less redraws

--- src/models/axis.js
+++ src/models/axis.js
@@ -45,6 +45,8 @@ var AxisModel = Hook.extend({
 
     //here the modified min and max may change the domain, if the scale is defined
     if(this.scale && this._readyOnce && this.use === "indicator") {
+      var obj = {};
+      
       if(this.scaleType == "time") {
         
         var timeMdl = this._space.time;
@@ -58,20 +60,23 @@ var AxisModel = Hook.extend({
         }
         
         //restore the correct object type for time values
-        if(this.zoomedMin != null && !utils.isDate(this.zoomedMin)) this.zoomedMin = this._space.time.parseToUnit(this.zoomedMin.toString());
-        if(this.zoomedMax != null && !utils.isDate(this.zoomedMax)) this.zoomedMax = this._space.time.parseToUnit(this.zoomedMax.toString());
+        if(this.zoomedMin != null && !utils.isDate(this.zoomedMin)) obj.zoomedMin = this._space.time.parseToUnit(this.zoomedMin.toString());
+        if(this.zoomedMax != null && !utils.isDate(this.zoomedMax)) obj.zoomedMax = this._space.time.parseToUnit(this.zoomedMax.toString());
         
-        if(!utils.isDate(this.domainMin)) this.domainMin = this.scale.domain()[0];
-        if(!utils.isDate(this.domainMax)) this.domainMax = this.scale.domain()[1];
+        if(!utils.isDate(this.domainMin)) obj.domainMin = this.scale.domain()[0];
+        if(!utils.isDate(this.domainMax)) obj.domainMax = this.scale.domain()[1];
       }
       //min and max nonsense protection
-      if(this.domainMin == null || this.domainMin <= 0 && this.scaleType === "log") this.domainMin = this.scale.domain()[0];
-      if(this.domainMax == null || this.domainMax <= 0 && this.scaleType === "log") this.domainMax = this.scale.domain()[1];
+      if(this.domainMin == null || this.domainMin <= 0 && this.scaleType === "log") obj.domainMin = this.scale.domain()[0];
+      if(this.domainMax == null || this.domainMax <= 0 && this.scaleType === "log") obj.domainMax = this.scale.domain()[1];
 
       //zoomedmin and zoomedmax nonsense protection    
-      if(this.zoomedMin == null) this.zoomedMin = this.scale.domain()[0];
-      if(this.zoomedMax == null) this.zoomedMax = this.scale.domain()[1];
+      if(this.zoomedMin == null) obj.zoomedMin = this.scale.domain()[0];
+      if(this.zoomedMax == null) obj.zoomedMax = this.scale.domain()[1];
 
+      //setting all validated parameters at once
+      this.set(obj);
+      
       this.scale.domain([this.domainMin, this.domainMax]);
     }
   },
