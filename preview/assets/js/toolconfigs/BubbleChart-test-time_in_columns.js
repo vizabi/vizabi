9f7090ab9527f419214d179eeebc6d2a0d1428b0
Angie Skazka <konzeptmeister@gmail.com>
2016-12-29 14:50:55 +0100

    add a test case time-in-columns-education

--- preview/assets/js/toolconfigs/BubbleChart-test-time_in_columns.js
+++ preview/assets/js/toolconfigs/BubbleChart-test-time_in_columns.js
@@ -1,6 +1,8 @@
 var VIZABI_MODEL = {
   "data": {
     "reader": "csv-time_in_columns",
-    "path": "data/waffles/time-in-columns.csv"
+//    "path": "data/waffles/time-in-columns.csv"
+    "delimiter": ";",
+    "path": "data/waffles/time-in-columns-education.csv"
   },
 };
