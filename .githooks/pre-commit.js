c33786b2be2244b7b2d56d85ca6f6f2f8fa36c92
Vladislav Baranchuk <kuguarpwnz@gmail.com>
2017-02-20 16:23:07 +0200

    Add git hook for eslint (#2431)
    * Add git hook for eslint
    
    * Fix pre-commit

---
+++ .githooks/pre-commit.js
@@ -0,0 +1,15 @@
+require('shelljs');
+
+const FIX = process.env.FIX ? '-- --fix' : '';
+const silent = true;
+
+const modifiedFiles = exec('git diff-index --name-only HEAD', { silent })
+  .grep(/\.js$/)
+  .stdout.replace(/[\n\r]/g, ' ');
+
+const result = exec(`npm run lint ${FIX} ${modifiedFiles}`, { silent });
+
+if (result.code) {
+  echo(result.stdout);
+  exit(result.code);
+}
