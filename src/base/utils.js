e4e661e050cca3ed784b940c1f33ce3cbfce265c
Vladislav Baranchuk <kgrpwnz@gmail.com>
2017-06-02 10:42:51 +0300

    Fix jsdoc for getProp helper

--- src/base/utils.js
+++ src/base/utils.js
@@ -1551,10 +1551,10 @@ export const isFunction = value => typeof value === "function";
  *   && this.show[dimension]["$in"].indexOf(d[dimension]) !== -1;
  * when you need to get (+check) nested properties.
 
- * @param {Array} names of properties for nesting
- * @param {context} the root object where we start to look for the props
- * @param {*} defaultValue default value that will be returned if there is no such properties in object
- * @returns {property} the property we're looking for or a default value
+ * @param {Array} props Names of properties for nesting
+ * @param {context} object The root object where we start to look for the props
+ * @param {*} defaultValue Default value that will be returned if there is no such properties in object
+ * @returns {property} The property we're looking for or a default value
 
  * Usage:
  * const object = { one: { two: "your value" } };
