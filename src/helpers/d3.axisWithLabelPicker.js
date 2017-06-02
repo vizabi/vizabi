6fd4189807c7bdf31862753948851367d33782fc
Angie the Fairytale <konzeptmeister@gmail.com>
2017-06-02 13:50:12 +0200

    Linter err

--- src/helpers/d3.axisWithLabelPicker.js
+++ src/helpers/d3.axisWithLabelPicker.js
@@ -413,7 +413,7 @@ export default function axisSmart(_orient) {
       // estimate the longest formatted label in pixels
       const estLongestLabelLength =
         //take 17 sample values and measure the longest formatted label
-        d3.max(d3.range(min, max, (max - min) / 17).concat(max).map(d => options.formatter(d).replace(".","").length)) * options.widthOfOneDigit + parseInt(options.cssMargin.left);
+        d3.max(d3.range(min, max, (max - min) / 17).concat(max).map(d => options.formatter(d).replace(".", "").length)) * options.widthOfOneDigit + parseInt(options.cssMargin.left);
 
       const pivot = options.isPivotAuto && (
         (estLongestLabelLength > options.pivotingLimit) && (orient == VERTICAL)
