1424ae67183770ec2443e6dd6bbea4a4a315dc0c
Jasper <github@jasperheeffer.nl>
2017-06-18 18:59:22 +0200

    Pass whole plain data object to reader as config
    Prevents us from sending irrelevant config to readers (delimiter to waffle/ddf for example) and no need to update core code for new reader configs

--- src/models/data.js
+++ src/models/data.js
@@ -168,13 +168,7 @@ const DataModel = Model.extend({
       throw new Error("Unknown reader: " + this.reader);
     }
 
-    return new readerClass({
-      path: this.path,
-      delimiter: this.delimiter,
-      keySize: this.keySize,
-      data: this.data,
-      endpoint: this.endpoint,
-    });
+    return new readerClass(this.getPlainObject());
   },
 
   checkQueryResponse(query, response) {
