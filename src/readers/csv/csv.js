1a90feefd8caec4924216f8d95ee6f3845bb7aba
Vladislav Baranchuk <kgrpwnz@gmail.com>
2017-07-17 14:15:19 +0300

    Tweak

--- src/readers/csv/csv.js
+++ src/readers/csv/csv.js
@@ -170,7 +170,7 @@ const CSVReader = Reader.extend({
 
   _createParseStrategy(separators) {
     return value => {
-      const hasOnlyNumbersOrSeparators = !/[^\d,.]/.test(value);
+      const hasOnlyNumbersOrSeparators = !(new RegExp(`[^\\d${separators}]`).test(value));
 
       if (hasOnlyNumbersOrSeparators && value) {
         const result = parseDecimal(value, separators);
