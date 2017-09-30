cf07257c1117b7ae20432e691b5d71ebc5259c29
Dmitry Basov <dmbasov@gmail.com>
2017-09-30 23:31:27 +0700

    IE | Upon positioning the mouse pointer on the any region, it doesn’t display any name like in other browsers #2764 (#2773)

--- src/components/colorlegend/colorlegend.js
+++ src/components/colorlegend/colorlegend.js
@@ -422,7 +422,7 @@ const ColorLegend = Component.extend({
             d3.select(this)
               .attr("d", shapeString)
               .style("fill", cScale(d[_this.colorlegendDim]))
-              .append("title").html(_this.frame.label[d[_this.colorlegendDim]]);
+              .append("title").text(_this.frame.label[d[_this.colorlegendDim]]);
 
             tempdivEl.html("");
           });
