c73eada55a49fc855c7930b780520dbf13c53fb3
Angie the Fairytale <konzeptmeister@gmail.com>
2018-01-28 17:26:13 +0100

    Improvement following 054affe: color model should not transfer constant which ("_default") to the dim of an entity

--- src/models/color.js
+++ src/models/color.js
@@ -151,7 +151,7 @@ const ColorModel = Hook.extend({
       marker.setDataSourceForAllSubhooks(this.data);
       entities.set(newFilter, false, false);
     } else {
-      if (model.isDiscrete() && model.use !== "constant") model.set({ which: this.which, data: this.data }, false, false);
+      if (model.isDiscrete()) model.set({ which: this.which, data: this.data }, false, false);
     }
   },
 
