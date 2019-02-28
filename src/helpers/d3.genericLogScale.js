73d5b9aa2f496532bcd98cfe12a57dded6e7af51
Dmitry Basov <dmbasov@gmail.com>
2019-02-28 16:55:13 +0700

    clean genericLog

--- src/helpers/d3.genericLogScale.js
+++ src/helpers/d3.genericLogScale.js
@@ -1,31 +1,4 @@
 //  d3.scaleGenericlog
 export default function genericLog() {
-  return (function d3_scale_genericLog(scale) {
-    const _this = this;
-    const defaultConstant = 0.1;
-
-    const abs = function(arg) {
-      if (arg instanceof Array)
-        return arg.map(d => Math.abs(d));
-      return Math.abs(arg);
-    };
-
-    const scaleDomain = scale.domain;
-    scale.domain = function(domain) {
-      if (!arguments.length) {
-        return scaleDomain();
-      }
-      const min = d3.min(abs(domain).filter(val => !!val));
-      //if (min) scale.constant(Math.min(defaultConstant, min / 100));
-
-      return scaleDomain(domain);
-    };
-
-    const scaleCopy = scale.copy;
-    scale.copy = function() {
-      return d3_scale_genericLog(scaleCopy());
-    };
-
-    return scale;
-  })(d3.scaleSymlog().constant(0.1));
+  return d3.scaleSymlog().constant(0.1);
 }
